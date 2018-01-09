923d24b474aa63d608cdfced3f9a30477be62aaf
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2018-01-09 15:03:00 +0100

    meteoJS: Nun eine JS-Datei (nicht mehr jsdoc)

--- src/meteoJS.js
+++ src/meteoJS.js
@@ -6,9 +6,10 @@
  * @todo für thermodynamicDiagram
  * Generator für einen Luftpaketaufstieg
  * Flächen zwischen 2 Soundings (für CAPE oder Unterschied der Temp./Feuchtigkeit.)
- * meteoJS initialisieren
- * Winde müssen auch integirert werden (ev. auch Windrose)
+ * Hodograph
  * Zusätzliche Angaben zu Messwerten (bsp. bei PSEUDO-Soundings die Wetterstation)
  * jQuery-Plugin
  * Tests
  */
+
+var meteoJS = meteoJS ? meteoJS : {};
