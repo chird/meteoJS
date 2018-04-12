7597ee705054f2b712556dd3bcf8b189a0661fed
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2018-04-12 08:56:05 +0200

    Added meteoJS/calc

---
+++ src/meteoJS/calc.js
@@ -0,0 +1,459 @@
+/**
+ * @module meteoJS/calc
+ */
+
+/**
+ * This Namespace contains physical respectively thermodynamic calculations.
+ * @namespace
+ */
+meteoJS.calc = meteoJS.calc ? meteoJS.calc : {};
+
+/**
+ * Umrechnung eines Luftdrucks zur entsprechenden Höhe in der
+ * Internationalen Standard-Atmosphäre (ISA).
+ * 
+ * Formel ist gültig bis 11 km Höhe
+ * (https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel#Internationale_H.C3.B6henformel)
+ * 
+ * @static
+ * @public
+ * @param {number|undefined} p Luftdruck [hPa]
+ * @return {number|undefined} Höhe in der Standard-Atmosphäre [m ü.M.]
+ */
+meteoJS.calc.altitudeISAByPres = function (p) {
+  if (p === undefined ||
+      isNaN(p))
+    return undefined;
+  return 44330.769*(1 - Math.pow(p/1013.25, 0.19029496));
+};
+
+/**
+ * Umrechnung einer Höhe in einen Luftdruck in der
+ * Internationalen Standard-Atmosphäre (ISA).
+ * 
+ * Formel ist gültig bis 11 km Höhe
+ * (https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel#Internationale_H.C3.B6henformel)
+ * 
+ * @static
+ * @public
+ * @param {number|undefined} a Höhe [m ü.M.]
+ * @return {number|undefined} Luftdruck in der Standard-Atmosphäre [hPa]
+ */
+meteoJS.calc.pressureISAByAltitude = function (a) {
+  if (a === undefined ||
+      isNaN(a))
+    return undefined;
+  return 1013.25*Math.pow(1-a/44330.769, 5.255);
+};
+
+/**
+ * Berechnung der potentielle Temperatur θ aus der Temperatur und des Drucks
+ * eines Luftpakets.
+ * 
+ * Berechnung zum Normaldruck von 1000 hPa
+ * (https://de.wikipedia.org/wiki/Potentielle_Temperatur#Trockenpotentielle_Temperatur)
+ * 
+ * @param {number|undefined} temp Temperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Potentielle Temperatur [K]
+ */
+meteoJS.calc.potentialTempByTempAndPres = function (temp, pres) {
+  if (temp === undefined || isNaN(temp) ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  return temp * Math.pow(1000/pres, 0.286);
+};
+
+/**
+ * Berechnung der Lufttemperatur bei einem Luftdruck für ein Luftpaket mit
+ * entsprechender potentiellen Temperatur.
+ * 
+ * Berechnung zum Normaldurck von 1000 hPa
+ * 
+ * @param {number|undefined} potentialTemp Potentielle Temperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Temperatur [K]
+ */
+meteoJS.calc.tempByPotentialTempAndPres = function (potentialTemp, pres) {
+  if (potentialTemp === undefined || isNaN(potentialTemp) ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  return potentialTemp*Math.pow(pres/1000, 0.286);
+};
+
+/**
+ * Temperatur eines Luftpaktes mit Druck 'pres' und der Äquivalent potentiellen Temperatur 'thetae'.
+ * 
+ * @param {number|undefined} thetae Äquivalent potentielle Temperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Temperatur [K]
+ */
+meteoJS.calc.tempByEquiPotTempAndPres = function (thetae, pres) {
+  if (pres === undefined || isNaN(pres))
+    return undefined;
+  var s = undefined;
+  var th = undefined;
+  var pcon = Math.pow(1000/pres, .286);
+  var t = 273.0;
+  var delta = 20;
+  var i = 0;
+  while (Math.abs(delta) > 0.1 && i < 100) {
+    i++;
+    s = meteoJS.calc.saturationHMRByTempAndPres(t, pres);
+    th = t * pcon * Math.exp(2.5*s/t);
+    if ((th-thetae)*delta > 0.0)
+      delta = -.5 * delta;
+    t = t + delta;
+  }
+  return t;
+};
+
+/**
+ * Taupunkt eines Luftpaktes über Mischungsverhältnis und Druck.
+ * 
+ * @param {number|undefined} hmr Mischungsverhätlnis [g/kg]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Taupunkttemperatur [K]
+ */
+meteoJS.calc.dewpointByHMRAndPres = function (hmr, pres) {
+  if (hmr === undefined || isNaN(hmr) ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  var x = 0.4343 * Math.log(hmr * pres /(622. + hmr));
+  return Math.pow(10,.0498646455 * x + 2.4082965 ) -
+         7.07475 +
+         38.9114 * Math.pow((Math.pow(10, .0915 * x) - 1.2035),2);
+};
+
+/**
+ * Wetbulb-Temperature, Psychro-Temperature aus Temperatur, Taupunkt und Luftdruck
+ * 
+ * @param {number|undefined} temp Temperatur [K]
+ * @param {number|undefined} dewpoint Taupunkttemperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Wetbulb-Temperatur [K]
+ */
+meteoJS.calc.wetbulbTempByTempAndDewpointAndPres = function (temp, dewpoint, pres) {
+  if (temp === undefined || isNaN(temp) ||
+      dewpoint === undefined || isNaN(dewpoint) ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  var result = dewpoint-273.15;
+  var Ediff = 1;
+  var incr = 10;
+  var previoussign = 1;
+  var E = 6.112 * Math.exp((17.67 * result) / (result + 243.5));
+  while (Math.abs(Ediff) > 0.05) {
+    var Eguess = 6.112 * Math.exp((17.67 * result) / (result + 243.5)) -
+                 pres * (temp-273.15 - result) * 0.00066 * (1 + (0.00115 * result));
+    Ediff = E - Eguess;
+    if (Ediff == 0)
+      break;
+    else {
+      if (Ediff < 0) {
+        if (-1 != previoussign) {
+          previoussign = -1;
+          incr = incr/10;
+        }
+      }
+      else {
+        if (1 != previoussign) {
+          previoussign = 1;
+          incr = incr/10;
+        }
+      }
+    }
+    if (Math.abs(Ediff) <= 0.05)
+      break;
+    else
+      result = result + incr*previoussign;
+  }
+  return result+273.15;
+};
+
+/**
+ * Äquivalent Potentielle Temperatur eines Luftpaktes.
+ * 
+ * Gemäss Formel von Holten (https://en.wikipedia.org/wiki/Equivalent_potential_temperature#Formula)
+ * 
+ * @param {number|undefined} temp Temperatur [K]
+ * @param {number|undefined} dewpoint Taupunkts-Temperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Äquivalent potentielle Temperatur [K]
+ */
+meteoJS.calc.equiPotentialTempByTempAndDewpointAndPres = function (temp, dewpoint, pres) {
+  var potTemp = meteoJS.calc.potentialTempByTempAndPres(temp, pres);
+  if (potTemp === undefined ||
+      dewpoint === undefined || isNaN(dewpoint) ||
+      temp === undefined || isNaN(temp) ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  return potTemp *
+    Math.exp(2481.9e-3 *
+             meteoJS.calc.saturationHMRByTempAndPres(dewpoint, pres) /
+             meteoJS.calc.lclTemperatureByTempAndDewpoint(temp, dewpoint));
+};
+
+/**
+ * Berechnung des Sättigung-Dampfdrucks zu einer Temperatur
+ * 
+ * @param {number|undefined} temp Temperatur [K]
+ * @return {number|undefined} Sättigungs-Dampfdruck [hPa]
+ */
+meteoJS.calc.saturationPressureByTemp = function (temp) {
+  if (temp === undefined || isNaN(temp))
+    return undefined;
+  var coef= new Array(6.1104546, 0.4442351, 1.4302099e-2, 2.6454708e-4, 3.0357098e-6, 2.0972268e-8, 6.0487594e-11,-1.469687e-13);
+  var inx=0;
+  // sat vap pressures every 5C from -50 to -200
+  var escold= new Array(
+    0.648554685769663908E-01, 0.378319512256073479E-01,
+    0.222444934288790197E-01, 0.131828928424683120E-01,
+    0.787402077141244848E-02, 0.473973049488473318E-02,
+    0.287512035504357928E-02, 0.175743037675810294E-02,
+    0.108241739518850975E-02, 0.671708939185605941E-03,
+    0.419964702632039404E-03, 0.264524363863469876E-03,
+    0.167847963736813220E-03, 0.107285397631620379E-03,
+    0.690742634496135612E-04, 0.447940489768084267E-04,
+    0.292570419563937303E-04, 0.192452912634994161E-04,
+    0.127491372410747951E-04, 0.850507010275505138E-05,
+    0.571340025334971129E-05, 0.386465029673876238E-05,
+    0.263210971965005286E-05, 0.180491072930570428E-05,
+    0.124607850555816049E-05, 0.866070571346870824E-06,
+    0.605982217668895538E-06, 0.426821197943242768E-06,
+    0.302616508514379476E-06, 0.215963854234913987E-06,
+    0.155128954578336869E-06);
+
+  temp = temp - 273.15;
+  var retval = 0;
+  //try {
+  if (temp > -50.) {
+    retval = ( coef[0] + temp*(coef[1] + temp*(coef[2] + temp*(coef[3] +
+    temp*(coef[4] + temp*(coef[5] + temp*(coef[6] + temp*coef[7])))))));
+
+  } else {
+     var tt = (-temp - 50.)/5.;
+     //var = (int) tt;
+     if (inx < escold.length-1) {
+       retval = escold[inx] + (tt % 1.)*(escold[inx+1]-escold[inx]);
+     } else {
+       retval = 1e-7;
+     }
+  }
+ // } catch (Exception e) {
+ //   GWT.log("caught exception: "+e);
+  //  retval = 1e-7;
+  //}
+  return retval;
+};
+
+/**
+ * Berechnung des Sättigungs-Mischungsverhältnisses aus Temperatur und Druck
+ * 
+ * HMR = humidity mixing ratio
+ * 
+ * @param {number|undefined} temp Temperatur [K]
+ * @param {number|undefined} pres Luftdruck [hPa]
+ * @return {number|undefined} Sättigungs-Mischungsverhältnis [g/kg]
+ */
+meteoJS.calc.saturationHMRByTempAndPres = function (temp, pres) {
+  var e = meteoJS.calc.saturationPressureByTemp(temp);
+  if (e === undefined ||
+      pres === undefined || isNaN(pres))
+    return undefined;
+  return 621.97*e/(pres - e);
+};
+
+/**
+ * Lifting Condensation Level (LCL) eines Luftpakets mit entsprechender
+ * potentieller Temperatur und Mischungsverhältnis.
+ * 
+ * @param {number|undefined} potentialTemp Potentielle Temperatur [K]
+ * @param {number|undefined} hmr Mischungsverhältnis [g/kg]
+ * @return {undefined|number} LCL [hPa]
+ */
+meteoJS.calc.lclByPotentialTempAndHMR = function (potentialTemp, hmr) {
+  if (hmr === undefined || isNaN(hmr))
+    return undefined;
+  // Binäre Suche
+  var a = 1013;
+  var b = 100;
+  while (a-b > 10) {
+    var p = b+(a-b)/2;
+    var hmrp = meteoJS.calc.saturationHMRByTempAndPres(
+                 meteoJS.calc.tempByPotentialTempAndPres(potentialTemp, p),
+                 p);
+    if (hmrp === undefined)
+      return undefined;
+    if (hmrp < hmr)
+      b = p;
+    else
+      a = p;
+  }
+  return b+(a-b)/2;
+};
+
+/**
+ * Potentielle Temperatur zu einem Lifting Condensation Level (LCL) bei
+ * gegebenem Mischungsverhältnis.
+ * 
+ * @param {number|undefined} lcl LCL [hPa]
+ * @param {number|undefined} hmr Mischungsverhältnis [g/kg]
+ * @return {number|undefined} Potentielle Temperatur [K]
+ */
+meteoJS.calc.potentialTempByLCLAndHMR = function (lcl, hmr) {
+  if (lcl === undefined || isNaN(lcl))
+    return undefined;
+  // Binäre Suche
+  var a = 323;
+  var b = 223;
+  while (a-b > 0.1) {
+    var Th = b+(a-b)/2;
+    var lclTh = meteoJS.calc.lclByPotentialTempAndHMR(Th, hmr);
+    if (lclTh === undefined)
+      return undefined;
+    if (lclTh > lcl)
+      b = Th;
+    else
+      a = Th;
+  }
+  return b+(a-b)/2;
+};
+
+/**
+ * Temperatur eines Luftpaktes, welches zum LCL angehoben wird.
+ * 
+ * @param {number|undefined} temp Lufttemperatur [K]
+ * @param {number|undefined} dewpoint Taupunktstemperatur [K]
+ * @return {undefined|number} Temperatur [K]
+ */
+meteoJS.calc.lclTemperatureByTempAndDewpoint = function (temp, dewpoint) {
+  if (temp === undefined || isNaN(temp) ||
+      dewpoint === undefined || isNaN(dewpoint))
+    return undefined;
+  return (dewpoint - (.001296*dewpoint - .15772)*(temp-dewpoint) );
+};
+
+/**
+ * Umwandlung Temperatur von Celsius in Kelvin
+ * 
+ * @param {number|undefined} temp [°C]
+ * @return {undefined|number} [K]
+ */
+meteoJS.calc.tempCelsiusToKelvin = function (temp) {
+  return (temp === undefined || isNaN(temp)) ? undefined : temp+273.15;
+};
+
+/**
+ * Umwandlung Temperatur von Kelvin zu Celsius
+ * 
+ * @param {number|undefined} temp [K]
+ * @return {undefined|number} [°C]
+ */
+meteoJS.calc.tempKelvinToCelsius = function (temp) {
+  return (temp === undefined || isNaN(temp)) ? undefined : temp-273.15;
+};
+
+/**
+ * Umwandlung Windgeschwindigkeit von m/s in km/h
+ * 
+ * @param {number|undefined} wind [m/s]
+ * @return {undefined|number} [km/h]
+ */
+meteoJS.calc.windspeedMSToKMH = function (wind) {
+  return (wind === undefined || isNaN(wind)) ? undefined : wind*3.6;
+};
+
+/**
+ * Umwandlung Windgeschwindigkeit von m/s in Knoten
+ * 
+ * @param {number|undefined} wind [m/s]
+ * @return {undefined|number} [kn]
+ */
+meteoJS.calc.windspeedMSToKN = function (wind) {
+  return (wind === undefined || isNaN(wind)) ? undefined : wind*900/463;
+};
+
+/**
+ * Umwandlung Windgeschwindigkeit von m/s in Beaufort
+ * 
+ * @param {number|undefined} wind [m/s]
+ * @return {undefined|number} [bf]
+ */
+meteoJS.calc.windspeedMSToBF = function (wind) {
+  return (wind === undefined || isNaN(wind)) ?
+    undefined :
+    Math.min(12, Math.pow(wind/0.8360, 2/3));
+};
+
+/**
+ * Abschätzung der Schneefallgrenze aus Temperatur und Höhe des 850hPa-Levels
+ * 
+ * Formel aus Weathercalc herauskopiert.
+ * 
+ * @static
+ * @public
+ * @param {number|undefined} temp Temperatur auf 850 hPa [K]
+ * @param {number|undefined} a Höhe des 850 hPa Levels [m ü.M.]
+ * @return {number|undefined} Abgeschätzte Schneefallgrenze [m ü.M.]
+ */
+meteoJS.calc.snowlineByTemp850hPaAndAltidude = function (temp, a) {
+  if (temp === undefined || isNaN(temp) ||
+      a === undefined || isNaN(a))
+    return undefined;
+  return 153.0 * meteoJS.calc.tempKelvinToCelsius(temp) + 0.9985 * a - 304.26;
+};
+
+/**
+ * Barometrische Höhenformel. Berechnung von Luftdruck in anderer Höhe.
+ * 
+ * Formel aus Wiki-Artikel https://de.wikipedia.org/wiki/Barometrische_H%C3%B6henformel
+ * 
+ * @static
+ * @public
+ * @param {number|undefined} p0 Druck auf dem Startlevel [hPa]
+ * @param {number|undefined} h Höhe bis zum Ziellevel (positiv für höher über Meereslevel) [m]
+ * @param {number|undefined} T0 Temperatur auf dem Startlevel [K]
+ * @param {number|undefined} T1 Temperatur auf dem Ziellevel, wenn undefined
+ *                              wird Isothermie angenommen [K]
+ * @return {number|undefined} Luftdruck auf dem Ziellevel [hPa]
+ */
+meteoJS.calc.pressureByBarometricFormula = function (p0, h, T0, T1) {
+  if (p0 === undefined || isNaN(p0) ||
+      h === undefined || isNaN(h) ||
+      T0 === undefined || isNaN(T0))
+    return undefined;
+  if (h == 0)
+    return p0;
+  var M = 0.02896;
+  var g = 9.807;
+  var R = 8.314;
+  if (T1 === undefined)
+    T1 = T0;
+  // Isotherme Atmosphäre
+  if (T0 == T1)
+    return p0 * Math.exp(-M*g/R/T0*h);
+  var a = (T0 - T1)/h;
+  return p0 * Math.exp(M*g/R/a*Math.log(1-a*h/T0));
+};
+
+/**
+ * Dichte von feuchter Luft.
+ * 
+ * Formel aus Wiki-Artikel https://en.wikipedia.org/wiki/Density_of_air
+ * 
+ * @param {number|undefined} p Luftdruck [hPa]
+ * @param {number|undefined} T Lufttemperatur [K]
+ * @param {number|undefined} rh Luftfeuchtigkeit []
+ * @return {number|undefined} Dichte [kg/m^3]
+ */
+meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity = function (p, T, rh) {
+  if (p === undefined || isNaN(p) ||
+      T === undefined || isNaN(T) ||
+      rh === undefined || isNaN(rh))
+    return undefined;
+  var Rd = 287.058; // J/(kg·K)
+  var Rv = 461.495; // J/(kg·K)
+  var pv = meteoJS.calc.saturationPressureByTemp(T)*rh;
+  return (p - pv)*100/Rd/T + pv*100/Rv/T;
+};
---
+++ tests/meteoJS/calc.test.js
@@ -0,0 +1,232 @@
+﻿QUnit.test("namespace", function (assert) {
+  assert.ok(meteoJS.calc, "namespace 'meteoJS.calc' exists"); 
+});
+QUnit.test("altitudeISAByPres", function (assert) {
+  assert.equal(meteoJS.calc.altitudeISAByPres(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.altitudeISAByPres(NaN), undefined,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.altitudeISAByPres("abc"), undefined,
+    "abc => undefined");
+  assert.equal(meteoJS.calc.altitudeISAByPres(0), 44330.769,
+    "0 hPa");
+  assert.equal(meteoJS.calc.altitudeISAByPres(1000), 110.90297084654266,
+    "1000 hPa");
+  assert.equal(meteoJS.calc.altitudeISAByPres(100), 15799.409730301008,
+    "100 hPa");
+  assert.equal(meteoJS.calc.altitudeISAByPres(850), 1457.5393962417284,
+    "850 hPa");
+});
+QUnit.test("pressureISAByAltitude", function (assert) {
+  assert.equal(meteoJS.calc.pressureISAByAltitude(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.pressureISAByAltitude(NaN), undefined,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.pressureISAByAltitude("abc"), undefined,
+    "abc => undefined");
+  assert.equal(meteoJS.calc.pressureISAByAltitude(100), 1001.296366796469,
+    "100 m");
+  assert.equal(meteoJS.calc.pressureISAByAltitude(16000), 96.36029976983998,
+    "16 km");
+  assert.equal(meteoJS.calc.pressureISAByAltitude(1500), 845.5855482103158,
+    "1500 m");
+});
+QUnit.test("potentialTempByTempAndPres", function (assert) {
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(NaN, NaN), undefined,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(0, 1000), 0,
+    "0,1000");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(10, 1000), 10,
+    "10,1000");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(-.5, 1000), -.5,
+    "-0.5,1000");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(-20, 500), -24.38510188152606,
+    "-20,500");
+  assert.equal(meteoJS.calc.potentialTempByTempAndPres(2, 500), 2.4385101881526063,
+    "2,500");
+});
+QUnit.test("tempByPotentialTempAndPres", function (assert) {
+  assert.equal(meteoJS.calc.tempByPotentialTempAndPres(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.tempByPotentialTempAndPres(NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("tempByEquiPotTempAndPres", function (assert) {
+  assert.equal(meteoJS.calc.tempByEquiPotTempAndPres(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.tempByEquiPotTempAndPres(NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("dewpointByHMRAndPres", function (assert) {
+  assert.equal(meteoJS.calc.dewpointByHMRAndPres(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.dewpointByHMRAndPres(NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("wetbulbTempByTempAndDewpointAndPres", function (assert) {
+  assert.equal(meteoJS.calc.wetbulbTempByTempAndDewpointAndPres(undefined, undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.wetbulbTempByTempAndDewpointAndPres(NaN, NaN, NaN), undefined,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.wetbulbTempByTempAndDewpointAndPres(1.3+273.15, 0.5+273.15, 976.6), 1.0+273.15,
+    "(1.3°C, 0.5°C, 976.6hPa) => 1.0°C");
+});
+QUnit.test("equiPotentialTempByTempAndDewpointAndPres", function (assert) {
+  assert.equal(meteoJS.calc.equiPotentialTempByTempAndDewpointAndPres(undefined, undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.equiPotentialTempByTempAndDewpointAndPres(NaN, NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("saturationPressureByTemp", function (assert) {
+  assert.equal(meteoJS.calc.saturationPressureByTemp(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.saturationPressureByTemp(NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("saturationHMRByTempAndPres", function (assert) {
+  assert.equal(meteoJS.calc.saturationHMRByTempAndPres(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.saturationHMRByTempAndPres(NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("lclByPotentialTempAndHMR", function (assert) {
+  assert.equal(meteoJS.calc.lclByPotentialTempAndHMR(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.lclByPotentialTempAndHMR(NaN, NaN), undefined,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.lclByPotentialTempAndHMR(0, 0), 103.56640625,
+    "NaN => undefined");
+  assert.equal(meteoJS.calc.lclByPotentialTempAndHMR(10, 20), 1009.43359375,
+    "NaN => undefined");
+});
+QUnit.test("lclTemperatureByTempAndDewpoint", function (assert) {
+  assert.equal(meteoJS.calc.lclTemperatureByTempAndDewpoint(undefined, undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.lclTemperatureByTempAndDewpoint(NaN, NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("tempCelsiusToKelvin", function (assert) {
+  assert.equal(meteoJS.calc.tempCelsiusToKelvin(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.tempCelsiusToKelvin(NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("tempKelvinToCelsius", function (assert) {
+  assert.equal(meteoJS.calc.tempKelvinToCelsius(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.tempKelvinToCelsius(NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("windspeedMSToKMH", function (assert) {
+  assert.equal(meteoJS.calc.windspeedMSToKMH(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.windspeedMSToKMH(NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("windspeedMSToKN", function (assert) {
+  assert.equal(meteoJS.calc.windspeedMSToKN(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.windspeedMSToKN(NaN), undefined,
+    "NaN => undefined");
+});
+QUnit.test("windspeedMSToBF", function (assert) {
+  assert.equal(meteoJS.calc.windspeedMSToBF(undefined), undefined,
+    "undefined => undefined");
+  assert.equal(meteoJS.calc.windspeedMSToBF(NaN), undefined,
+    "NaN => undefined");
+  assert.equal(
+    Math.round(meteoJS.calc.windspeedMSToBF(50)*10)/10,
+    12,
+    "40 m/s => 12 Bf");
+});
+QUnit.test("snowlineByTemp850hPaAndAltidude", function (assert) {
+  assert.equal(meteoJS.calc.snowlineByTemp850hPaAndAltidude(undefined, undefined), undefined,
+    "undef,undef => undefined");
+  assert.equal(meteoJS.calc.snowlineByTemp850hPaAndAltidude(NaN, NaN), undefined,
+    "NaN,NaN => undefined");
+  assert.equal(meteoJS.calc.snowlineByTemp850hPaAndAltidude("abc", "def"), undefined,
+    "abc,def => undefined");
+  assert.equal(
+    Math.round(meteoJS.calc.snowlineByTemp850hPaAndAltidude(meteoJS.calc.tempCelsiusToKelvin(3), 1500)*100)/100,
+    1652.49,
+    "3°C, 1500m");
+  assert.equal(
+    Math.round(meteoJS.calc.snowlineByTemp850hPaAndAltidude(meteoJS.calc.tempCelsiusToKelvin(-4), 1500)*100)/100,
+    581.49,
+    "-4°C, 1500m");
+  assert.equal(
+    Math.round(meteoJS.calc.snowlineByTemp850hPaAndAltidude(meteoJS.calc.tempCelsiusToKelvin(-4), 1600)*100)/100,
+    681.34,
+    "-4°C, 1600m");
+  assert.equal(
+    Math.round(meteoJS.calc.snowlineByTemp850hPaAndAltidude(meteoJS.calc.tempCelsiusToKelvin(-4), 1300)*100)/100,
+    381.79,
+    "-4°C, 1300m");
+});
+QUnit.test("pressureByBarometricFormula", function (assert) {
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(undefined, undefined, undefined), undefined,
+    "undef's => undefined");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(NaN, NaN, NaN), undefined,
+    "NaN's => undefined");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula("abc", "def", "ghi"), undefined,
+    "strings's => undefined");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(1024, 0, 273.15), 1024,
+    "Höhe 0m");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(950, 1000, 273.15), 838.3205294660723,
+    "Isotherm 1000 m");
+  assert.equal(Math.round(meteoJS.calc.pressureByBarometricFormula(1013.25, 100, 288.15, 287.5)*1000)/1000,
+    1001.295,
+    "Standardatmosphäre 100 m");
+  assert.equal(Math.round(meteoJS.calc.pressureByBarometricFormula(1013.25, 16000, 288.15, 184.15)*1000)/1000,
+    96.340,
+    "Standardatmosphäre 16 km");
+  assert.equal(Math.round(meteoJS.calc.pressureByBarometricFormula(1013.25, 1500, 288.15, 278.4)*1000)/1000,
+    845.572,
+    "Standardatmosphäre 1500 m");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(954.3, -500, 263.15, 266.4), 1017.8903862567589,
+    "500 m -> Reduktion auf Meereshöhe, -10°C");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(954.3, -500, 273.15, 276.4), 1015.5034127770334,
+    "500 m -> Reduktion auf Meereshöhe, 0°C");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(954.3, -500, 283.15, 286.4), 1013.2891061035103,
+    "500 m -> Reduktion auf Meereshöhe, 10°C");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(954.3, -500, 293.15, 296.4), 1011.22938945771,
+    "500 m -> Reduktion auf Meereshöhe, 20°C");
+  assert.equal(meteoJS.calc.pressureByBarometricFormula(954.3, -500, 303.15, 306.4), 1009.3086237305216,
+    "500 m -> Reduktion auf Meereshöhe, 30°C");
+});
+QUnit.test("potentialTempByLCLAndHMR", function (assert) {
+  assert.equal(meteoJS.calc.potentialTempByLCLAndHMR(undefined, undefined), undefined,
+    "undef's => undefined");
+  assert.equal(meteoJS.calc.potentialTempByLCLAndHMR(NaN, NaN), undefined,
+    "NaN's => undefined");
+  assert.equal(meteoJS.calc.potentialTempByLCLAndHMR("abc", "def"), undefined,
+    "strings's => undefined");
+  assert.equal(Math.round(meteoJS.calc.potentialTempByLCLAndHMR(700, 3)*100)/100, 293.75,
+    "700 hP, 3 g/kg => ~294K");
+  assert.equal(Math.round(meteoJS.calc.potentialTempByLCLAndHMR(900, 1)*100)/100,  262.70,
+    "900 hP, 1 g/kg => ~263K");
+  assert.equal(Math.round(meteoJS.calc.potentialTempByLCLAndHMR(500, 0.3)*100)/100, 287.21,
+    "500 hP, 0.3 g/kg => ~287K");
+});
+QUnit.test("densityHumidAirByPressureAndTempAndRelHumidity", function (assert) {
+  assert.equal(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(undefined, undefined, undefined), undefined,
+    "undef's => undefined");
+  assert.equal(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(NaN, NaN, NaN), undefined,
+    "NaN's => undefined");
+  assert.equal(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity("ab", "cd", "ef"), undefined,
+    "string's => undefined");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(1013.25, meteoJS.calc.tempCelsiusToKelvin(35), 0)*1000)/1000, 1.145,
+    "Trocken, Meeresoberfläche, 35°C => 1.145");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(1013.25, meteoJS.calc.tempCelsiusToKelvin(5), 0)*1000)/1000, 1.2690,
+    "Trocken, Meeresoberfläche, 5°C => 1.2690");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(1013.25, meteoJS.calc.tempCelsiusToKelvin(-20), 0)*1000)/1000, 1.394,
+    "Trocken, Meeresoberfläche, -20°C => 1.3943");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(1000, meteoJS.calc.tempCelsiusToKelvin(0), 0)*1000)/1000, 1.275,
+    "Trocken, 1000 hPa, 0°C => 1.2754");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(900, meteoJS.calc.tempCelsiusToKelvin(0), 1)*1000)/1000, 1.145,
+    "100%, 900 hPa, 0°C => 1.145");
+  assert.equal(Math.round(meteoJS.calc.densityHumidAirByPressureAndTempAndRelHumidity(900, meteoJS.calc.tempCelsiusToKelvin(0), 0.5)*1000)/1000, 1.146,
+    "50%, 900 hPa, 0°C => 1.146");
+});
