c4cc35be300beeee657aa1f140d0471ad2c67d6b
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2018-10-29 15:44:54 +0100

    Synview: Missed changes in previous commits

--- bin/make_release.pl
+++ bin/make_release.pl
@@ -50,7 +50,7 @@ my @lib_files = (
   "meteoJS/synview/map/ol.js",
   "meteoJS/synview/map/ll.js",
   "meteoJS/synview/resource.js",
-  "meteoJS/synview/resource/GeoImage.js",
+  "meteoJS/synview/resource/Image.js",
   "meteoJS/synview/resource/Vector.js",
   "meteoJS/synview/resource/GeoJSON.js",
   "meteoJS/synview/resource/GeoJSONTile.js",
