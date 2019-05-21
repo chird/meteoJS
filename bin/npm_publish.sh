730dda2aa539314a410ed553328f25ead5b65f3b
root <meteo@srf.ch>
2019-05-21 11:48:58 +0200

    ES6: npm publish script added

---
+++ bin/npm_publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp -R src/ build/
+if [ $? -eq 0 ]; then
+    echo "src/ copied to build/"
+    cp package.json build/
+    if [ $? -eq 0 ]; then
+        echo "package.json copied to build/"
+        cd build/
+        if [ $? -eq 0 ]; then
+            echo "chdir to build/"
+            npm publish
+            cd ..
+        else
+            echo "Couldn't chdir to build/"
+        fi
+    else
+        echo "Couldn't copy package.json to build/"
+    fi
+    rm -fR build/
+    if [ $? -eq 0 ]; then
+        echo "build/ deleted"
+    fi
+else
+    echo "Couldn't copy src/ to build/"
+fi
