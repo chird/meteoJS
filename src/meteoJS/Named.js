d257af7bb87ca0b75b8c60338fb7dd6bb4032890
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-09-12 12:06:26 +0200

    Untested first version of class "Named"

---
+++ src/meteoJS/Named.js
@@ -0,0 +1,66 @@
+/**
+ * @module meteoJS/named
+ */
+import extend from 'jquery-extend';
+
+/**
+ * Options for constructor.
+ * 
+ * @typedef {Object} meteoJS/named~options
+ * @param {Object.<string,string>} names Names.
+ * @param {string[]} [langSortation] Priority of language codes.
+ */
+
+/**
+ * @classdesc Class with a name in different languages.
+ */
+export class Named {
+  
+  /**
+   * @param {meteoJS/named~options} Options.
+   */
+  constructor(options) {
+    options = extend(true, {
+      names: undefined,
+      langSortation: undefined
+    }, options);
+    this.names = options.names === undefined ? {} : options.names;
+    this.langSortation =
+      options.langSortation === undefined ? {} : options.langSortation;
+  }
+  
+  /**
+   * @returns {string} Default name.
+   */
+  get name() {
+    return this.getNameByLang();
+  }
+  
+  /**
+   * @param {string} [lang] Language code.
+   * @returns {string} Name in the passed or a fallback language.
+   */
+  getNameByLang(lang) {
+    let langs = Object.keys(this.names);
+    if (langs.length < 1)
+      return '';
+    return this.names[langs.sort((a, b) => {
+      if (a == lang) return -1;
+      if (b == lang) return 1;
+      let ia = this.langSortation.indexOf(a);
+      let ib = this.langSortation.indexOf(b);
+      if (ib < 0) return 0;
+      if (ia < 0) return 1;
+      return ia < ib ? -1 : ia == ib ? 0 : 1;
+    })[0]];
+  }
+  
+  /**
+   * @param {string} lang Language code.
+   * @returns {string} Name in the passed language.
+   */
+  getNameByLangNoFallback(lang) {
+    return (lang in this.names)  ? this.names[lang] : '';
+  }
+}
+export default Named;
---
+++ test/meteoJS/named.test.js
@@ -0,0 +1,84 @@
+﻿import Named from 'Named.js';
+
+QUnit.test("Empty class", function (assert) {
+  let n = new Named();
+  assert.equal(n.name, '', 'name');
+  assert.equal(n.getNameByLang(), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('en'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('de'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('fr'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('rm'), '', 'getNameByLang');
+  assert.equal(n.getNameByLangNoFallback('en'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('de'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('fr'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('rm'), '', 'getNameByLangNoFallback');
+});
+QUnit.test("No languages", function (assert) {
+  let n = new Named({
+    names: {}
+  });
+  assert.equal(n.name, '', 'name');
+  assert.equal(n.getNameByLang(), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('en'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('de'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('fr'), '', 'getNameByLang');
+  assert.equal(n.getNameByLang('rm'), '', 'getNameByLang');
+  assert.equal(n.getNameByLangNoFallback('en'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('de'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('fr'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('rm'), '', 'getNameByLangNoFallback');
+});
+QUnit.test("One language", function (assert) {
+  let n = new Named({
+    names: {
+      de: 'Test'
+    }
+  });
+  assert.equal(n.name, 'Test', 'name');
+  assert.equal(n.getNameByLang(), 'Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('en'), 'Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('de'), 'Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('fr'), 'Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('rm'), 'Test', 'getNameByLang');
+  assert.equal(n.getNameByLangNoFallback('en'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('de'), 'Test', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('fr'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('rm'), '', 'getNameByLangNoFallback');
+});
+QUnit.test("Two languages", function (assert) {
+  let n = new Named({
+    names: {
+      en: 'en-Test',
+      de: 'de-Test'
+    }
+  });
+  assert.equal(n.name, 'en-Test', 'name');
+  assert.equal(n.getNameByLang(), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('en'), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('de'), 'de-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('fr'), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('rm'), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLangNoFallback('en'), 'en-Test', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('de'), 'de-Test', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('fr'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('rm'), '', 'getNameByLangNoFallback');
+});
+QUnit.test("Two languages, adjusted sortation", function (assert) {
+  let n = new Named({
+    names: {
+      en: 'en-Test',
+      de: 'de-Test'
+    },
+    langSortation: ['de', 'en']
+  });
+  assert.equal(n.name, 'de-Test', 'name');
+  assert.equal(n.getNameByLang(), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('en'), 'en-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('de'), 'de-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('fr'), 'de-Test', 'getNameByLang');
+  assert.equal(n.getNameByLang('rm'), 'de-Test', 'getNameByLang');
+  assert.equal(n.getNameByLangNoFallback('en'), 'en-Test', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('de'), 'de-Test', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('fr'), '', 'getNameByLangNoFallback');
+  assert.equal(n.getNameByLangNoFallback('rm'), '', 'getNameByLangNoFallback');
+});
