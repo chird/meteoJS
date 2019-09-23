2bd461f8ba7248932b4baefad5ef987535b4f7f8
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-09-23 15:43:31 +0200

    Added tests to VariableCollection

--- src/meteoJS/modelviewer/VariableCollection.js
+++ src/meteoJS/modelviewer/VariableCollection.js
@@ -1,34 +1,62 @@
 /**
  * @module meteoJS/modelviewer/variableCollection
  */
-import NamedCollection from '../NamedCollection.js';
+import Variable from './Variable.js';
+import NamedCollection from '../base/NamedCollection.js';
 
 /**
- * Options for VariableCollection constructor.
+ * Triggered on adding a Variable to collection.
  * 
- * @typedef {meteoJS/namedCollection~options}
+ * @event meteoJS/modelviewer/variableCollection#add:variable
+ * @param {module:meteoJS/modelviewer/variable.Variable} variable -
+ *   Added variable.
+ */
+
+/**
+ * Triggered on removing a Variable from collection.
+ * 
+ * @event meteoJS/modelviewer/variableCollection#remove:variable
+ * @param {module:meteoJS/modelviewer/variable.Variable} variable -
+ *   Removed variable.
+ */
+
+/**
+ * Options for constructor.
+ * 
+ * @typedef {module:meteoJS/base/namedCollection~options}
  *   meteoJS/modelviewer/variableCollection~options
  */
 
 /**
- * @classdesc A collection of variable objects.
- * @fires add:variable
- * @fires remove:variable
+ * @classdesc A collection of Variable-objects.
+ * @fires meteoJS/modelviewer/variableCollection#add:variable
+ * @fires meteoJS/modelviewer/variableCollection#remove:variable
  */
 export class VariableCollection extends NamedCollection {
   
   /**
    * @param {meteoJS/modelviewer/variableCollection~options} options - Options.
    */
-  constructor(options) {
+  constructor({ fireReplace=true,
+                fireAddRemoveOnReplace=false,
+                appendOnReplace=true,
+                sortFunction,
+                names,
+                langSortation } = {}) {
     super({
-      makeEmptyObject: () => new Variable()
+      emptyObjectMaker: () => { return new Variable() },
+      fireReplace,
+      fireAddRemoveOnReplace,
+      appendOnReplace,
+      sortFunction,
+      names,
+      langSortation
     });
     
     this.on('add:item', item => this.trigger('add:variable', item));
     this.on('remove:item', item => this.trigger('remove:variable', item));
   }
   
-  sortation()...
+  //sortation()...
 }
 export default VariableCollection;
---
+++ test/meteoJS/modelviewer/variablecollection.test.js
@@ -0,0 +1,55 @@
+﻿const assert = require("assert");
+import Variable from '../../../src/meteoJS/modelviewer/Variable.js';
+import VariableCollection
+  from '../../../src/meteoJS/modelviewer/VariableCollection.js';
+import { VariableCollection as VariableCollectionClass }
+  from '../../../src/meteoJS/modelviewer/VariableCollection.js';
+
+describe('Default VariableCollection, import via default', () => {
+  describe('Triggered events', () => {
+    let addCounter = 0;
+    let removeCounter = 0;
+    let a = new Variable({ id: 'a' });
+    let b = new Variable({ id: 'b' });
+    let vars = new VariableCollection();
+    vars.on('add:variable', v => addCounter++);
+    vars.on('remove:variable', v => removeCounter++);
+    vars.append(a).append(b).remove(a);
+    it('event counters', () => {
+      assert.equal(addCounter, 2);
+      assert.equal(removeCounter, 1);
+    });
+  });
+  describe('empty object', () => {
+    let vars = new VariableCollection();
+    let v = vars.getItemById('NotExistant');
+    assert.ok(v instanceof Variable);
+  });
+  describe('inherited methods', () => {
+    let vars = new VariableCollection({
+      names: {
+        de: 'de-Test',
+        en: 'en-Test'
+      }
+    });
+    it('names', () => {
+      assert.equal(vars.name, 'de-Test');
+      assert.equal(vars.getNameByLang('en'), 'en-Test');
+    });
+    it('collection', () => {
+      vars.append(new Variable());
+      assert.equal(vars.count, 1);
+    });
+  });
+});
+describe('VariableCollection class, import via name', () => {
+  describe('simple', () => {
+    let a = new Variable({ id: 'a' });
+    let b = new Variable({ id: 'b' });
+    let vars = new VariableCollectionClass();
+    vars.append(a,b);
+    it('count', () => {
+      assert.equal(vars.count, 2);
+    });
+  });
+});
