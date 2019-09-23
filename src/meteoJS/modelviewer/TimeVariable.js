bad955f4c16229165a76edbbf2ceffee3e0035e1
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-09-23 13:24:55 +0200

    Fixed TimeVariable and switched test to mocha

--- src/meteoJS/modelviewer/TimeVariable.js
+++ src/meteoJS/modelviewer/TimeVariable.js
@@ -2,41 +2,45 @@
  * @module meteoJS/modelviewer/timeVariable
  */
 import extend from 'jquery-extend';
-import Variable from 'Variable.js';
+import Variable from './Variable.js';
 
 /**
  * Options for TimeVariable constructor
  * 
- * @typedef {meteoJS/modelviewer/variable~options}
+ * @typedef {meteoJS/base/uniquenamed~options}
  *   meteoJS/modelviewer/timeVariable~options
  * @param {Date|undefined} [datetime] - Datetime.
  */
 
 /**
- * @classdesc Object for something representing a time, e.g. runtime or offset.
+ * @classdesc Class for something representing a time, e.g. runtime or offset.
  */
 export class TimeVariable extends Variable {
   
   /**
    * @param {meteoJS/modelviewer/timeVariable~options} options Options.
    */
-  constructor(options) {
+  constructor(options = {}) {
     options = extend(true, {
       datetime: undefined
     }, options);
     super(options);
     
-    /** @type Date|undefined */
-    this._datetime = undefined;
+    /**
+     * @type Date|undefined
+     * @private
+     */
+    this._datetime = (this.id === undefined) ? undefined : new Date(this.id);
     
-    this.datetime = options.datetime;
+    if (options.datetime !== undefined)
+      this.datetime = options.datetime;
   }
   
   /**
    * @override
    */
-  set id(id) {
-    this.datetime = (id === undefined) ? undefined : new Date(id);
+  setId(id) {
+    this._datetime = (id === undefined) ? undefined : new Date(id);
   }
   
   /**
@@ -47,7 +51,7 @@ export class TimeVariable extends Variable {
   }
   set datetime(datetime) {
     this._datetime = datetime;
-    super.id = (datetime !== undefined) ? datetime.valueOf() : undefined;
+    this._id = (datetime !== undefined) ? datetime.valueOf() : undefined;
   }
 }
-export default TimeVariable
+export default TimeVariable;
--- test/meteoJS/modelviewer/timevariable.test.js
+++ test/meteoJS/modelviewer/timevariable.test.js
@@ -1,34 +1,71 @@
-﻿import Named from 'modelviewer/TimeVariable.js';
+﻿const assert = require("assert");
+import TimeVariable from '../../../src/meteoJS/modelviewer/TimeVariable.js';
 
-QUnit.test("some tests", function (assert) {
+describe('TimeVariable class, import via default', () => {
   let d1 = new Date('2019-09-12T00:00:00');
   let d2 = new Date('2018-01-10T00:00:00');
-  let tv1 = new TimeVariable();
-  assert.equal(tv1.id, undefined, 'undefined id');
-  assert.equal(tv1.datetime, undefined, 'undefined datetime');
-  tv1.datetime = d1;
-  assert.equal(tv1.id, d1.valueOf(), 'id=d1');
-  assert.equal(tv1.datetime.valueOf(), d1.valueOf(), 'datetime=d1');
-  tv1.id = d2.valueOf();
-  assert.equal(tv1.id, d2.valueOf(), 'id=d2');
-  assert.equal(tv1.datetime.valueOf(), d2.valueOf(), 'datetime=d2');
-  tv1.datetime = undefined;
-  assert.equal(tv1.id, undefined, 'undefined id');
-  assert.equal(tv1.datetime, undefined, 'undefined datetime');
-  let tv2 = new TimeVariable({
-    datetime: d1
+  describe('empty constructor', () => {
+    describe('empty id/datetime', () => {
+      let tv = new TimeVariable();
+      it('id', () =>  {
+        assert.equal(tv.id, undefined);
+      });
+      it('datetime', () =>  {
+        assert.equal(tv.datetime, undefined);
+      });
+    });
+    describe('set datetime', () => {
+      let tv = new TimeVariable();
+      tv.datetime = d1;
+      it('id', () => {
+        assert.equal(tv.id, d1.valueOf())
+      });
+      it('datetime', () => {
+        assert.equal(tv.datetime, d1);
+      });
+    });
+    it('set id with valueOf(), empty again', () => {
+      let tv = new TimeVariable();
+      tv.id = d2.valueOf();
+      assert.equal(tv.id, d2.valueOf());
+      assert.equal(tv.datetime.valueOf(), d2.valueOf());
+      tv.datetime = undefined;
+      assert.equal(tv.id, undefined);
+      assert.equal(tv.datetime, undefined);
+    });
   });
-  assert.equal(tv2.id, d1.valueOf(), 'id=d1');
-  assert.equal(tv2.datetime.valueOf(), d1.valueOf(), 'datetime=d1');
-  let tv3 = new TimeVariable({
-    id: d1.valueOf(),
-    datetime: d2
+  describe('constructor with datetime', () => {
+    let tv2 = new TimeVariable({
+      datetime: d1
+    });
+    it('id=d1', () => {
+      assert.equal(tv2.id, d1.valueOf());
+    });
+    it('datetime=d1', () => {
+      assert.equal(tv2.datetime.valueOf(), d1.valueOf());
+    });
   });
-  assert.equal(tv3.id, d2.valueOf(), 'id=d2');
-  assert.equal(tv3.datetime.valueOf(), d2.valueOf(), 'datetime=d2');
-  let tv4 = new TimeVariable({
-    id: d1.valueOf()
+  describe('constructor with datetime and id', () => {
+    let tv3 = new TimeVariable({
+      id: d1.valueOf(),
+      datetime: d2
+    });
+    it('id=d2', () => {
+      assert.equal(tv3.id, d2.valueOf());
+    });
+    it('datetime=d2', () => {
+      assert.equal(tv3.datetime.valueOf(), d2.valueOf());
+    });
   });
-  assert.equal(tv4.id, d1.valueOf(), 'id=d1');
-  assert.equal(tv4.datetime.valueOf(), d1.valueOf(), 'datetime=d1');
+  describe('constructor with id', () => {
+    let tv4 = new TimeVariable({
+      id: d1.valueOf()
+    });
+    it('id=d1', () => {
+      assert.equal(tv4.id, d1.valueOf());
+    });
+    it('datetime=d1', () => {
+      assert.equal(tv4.datetime.valueOf(), d1.valueOf());
+    });
+  });
 });
