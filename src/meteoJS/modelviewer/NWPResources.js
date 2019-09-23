f68109d8b7d18d7b74d44245c8805946f53e3884
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-09-23 17:01:55 +0200

    NWPResources: Work on API

--- src/meteoJS/modelviewer/NWPResources.js
+++ src/meteoJS/modelviewer/NWPResources.js
@@ -3,43 +3,121 @@
  */
 
 /**
- * Kind-Klasse von Resources. Erstellt Resources direkt für NWP.
+ * @classdesc Setup a Resources-Object to be used for NWP.
  */
 export class NWPResources extends Resources {
   
-  constructor(options) {
-    let variableCollection = new VariableCollection({
+  constructor() {
+    let models = new VariableCollection({
       id: 'models'
     });
-    options.runs.forEach(id => {
-      variableCollection.append(new Variable({ id });
-    });
-    let nodeModels = new VariableCollectionNode({
-      variableCollection
+    super({
+      topVariableCollection: models
     });
     
-    nodeModels.appendNode(nodeRuns);
-    nodeRuns.appendNode(nodeFields);
-    nodeFields.appendNode(nodeLevels).appendNode(nodeAccumulations);
-    nodeOffset.parents(nodeLevels, nodeAccumulations);
+    let runs = new VariableCollection({
+      id: 'runs'
+    });
+    let regions = new VariableCollection({
+      id: 'regions'
+    });
+    let fields = new VariableCollection({
+      id: 'fields'
+    });
+    let levels = new VariableCollection({
+      id: 'levels'
+    });
+    let accumulations = new VariableCollection({
+      id: 'accumulations'
+    });
+    let offsets = new VariableCollection({
+      id: 'offsets'
+    });
+    models.appendChild(runs);
+    runs.appendChild(regions);
+    regions.appendChild(fields);
+    fields.appendChild(levels, accumulations);
+    levels.appendChild(offsets);
+    accumulations.appendChild(offsets);
   }
   
-  get models() {
+  /**
+   * Creates a Variable-Object and adds it to a VariableCollection.
+   * 
+   * @param {mixed} variableCollectionId - ID of the VariableCollection.
+   * @param {Object} [options] - Options.
+   * ...
+   * @returns {NWPResources} This.
+   */
+  addVariable(variableCollectionId, { id, names = {} } = {}) {
+    this.getVariableCollectionById(variableCollectionId).append(new Variable({
+      id,
+      names
+    }));
+    return this;
   }
   
-  get runs() {
+  /**
+   * Collection of all defined models.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get modelCollection() {
+    return this.topVariableCollection;
   }
   
-  get fields() {
+  /**
+   * Collection of all defined runs.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get runCollection() {
+    return this.getVariableCollectionById('runs');
   }
   
-  get levels() {
+  /**
+   * Collection of all defined regions.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get regionCollection() {
+    return this.getVariableCollectionById('regions');
   }
   
-  get accumulations() {
+  /**
+   * Collection of all defined fields.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get fieldCollection() {
+    return this.getVariableCollectionById('fields');
   }
   
-  get hierarchy() {
+  /**
+   * Collection of all defined levels.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get levelCollection() {
+    return this.getVariableCollectionById('levels');
+  }
+  
+  /**
+   * Collection of all defined accumulations.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get accumulationCollection() {
+    return this.getVariableCollectionById('accumulations');
+  }
+  
+  /**
+   * Collection of all defined offsets.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get offsetCollection() {
+    return this.getVariableCollectionById('offsets');
   }
 }
 export default NWPResources;
