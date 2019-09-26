c9036191f83118d3433ee4e0ed67e1fb91aec12b
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-09-26 15:13:03 +0200

    Renamed class to Node, added resources with tests

---
+++ src/meteoJS/modelviewer/Node.js
@@ -0,0 +1,151 @@
+/**
+ * @module meteoJS/modelviewer/node
+ */
+import addEventFunctions from '../Events.js';
+
+/**
+ * Triggered on append of a child node.
+ * 
+ * @event module:meteoJS/modelviewer/node#append:child
+ * @param {module:meteoJS/modelviewer/node.Node} node - Appended child node.
+ */
+
+/**
+ * A node has always a correspondent VariableCollection. With the node objects
+ * a hierarchy of the collections is build up. This hierarchy is mainly used
+ * to achieve a good user experience. This way, the menus in the
+ * {@link module:meteoJS/modelviewer/container.Container|modelviewer-container}
+ * could be setup to present the user only available fields or levels or only
+ * activate buttons which are available for a certain model and run.
+ * Additionally a node contains a list of resources, this is used internally
+ * in {@link module:meteoJS/modelviewer/resources.Resources}.
+ * 
+ * @fires module:meteoJS/modelviewer/node#append:child
+ */
+export class Node {
+  
+  /**
+   * @param {module:meteoJS/modelviewer/variableCollection.VariableCollection}
+   *   variableCollection
+   *   This node belongs to this collection.
+   */
+  constructor(variableCollection) {
+    
+    /**
+     * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+     * @private
+     */
+    this._variableCollection = variableCollection;
+    
+    /**
+     * @type module:meteoJS/modelviewer/node.Node[]
+     * @private
+     */
+    this._parents = [];
+    
+    /**
+     * @type module:meteoJS/modelviewer/node.Node[]
+     * @private
+     */
+    this._children = [];
+    
+    /**
+     * @type module:meteoJS/modelviewer/resource.Resource[]
+     * @private
+     */
+    this._resources = [];
+  }
+  
+  /**
+   * VariableCollection correspondent to this node.
+   * 
+   * @type module:meteoJS/modelviewer/variableCollection.VariableCollection
+   */
+  get variableCollection() {
+    return this._variableCollection;
+  }
+  
+  /**
+   * Parent nodes.
+   * 
+   * @type module:meteoJS/modelviewer/node.Node[]
+   */
+  get parents() {
+    return this._parents;
+  }
+  
+  /**
+   * Child nodes.
+   * 
+   * @type module:meteoJS/modelviewer/node.Node[]
+   */
+  get children() {
+    return this._children;
+  }
+  
+  /**
+   * Appends a node as a child.
+   * 
+   * @param {...module:meteoJS/modelviewer/node.Node} nodes - Node to append.
+   * @returns {module:meteoJS/modelviewer/node.Node} This.
+   * @fires module:meteoJS/modelviewer/node#append:child
+   */
+  appendChild(...nodes) {
+    nodes.forEach(node => {
+      this._children.push(node);
+      node._addParent(this);
+      this.trigger('append:child', node);
+    });
+    return this;
+  }
+  
+  /**
+   * Adds a parent node.
+   * 
+   * @param {module:meteoJS/modelviewer/node.Node} node - Node to add.
+   * @package
+   */
+  _addParent(node) {
+    this._parents.push(node);
+  }
+  
+  /**
+   * All contained resources.
+   * 
+   * @type module:meteoJS/modelviewer/resource.Resource[]
+   * @package
+   */
+  get resources() {
+    return this._resources;
+  }
+  
+  /**
+   * Append resources.
+   * 
+   * @param {...module:meteoJS/modelviewer/resource.Resource} resources
+   *   Resources.
+   * @package
+   */
+  append(...resources) {
+    resources.forEach(resource => {
+      this._resources.push(resource);
+    });
+  }
+  
+  /**
+   * Removes resources.
+   * 
+   * @param {...module:meteoJS/modelviewer/resource.Resource} resources
+   *   Resources.
+   * @package
+   */
+  remove(...resources) {
+    resources.forEach(resource => {
+      let i = this._resources.indexOf(resource);
+      if (i > -1)
+        this._resources.splice(i, 1);
+    });
+  }
+}
+addEventFunctions(Node.prototype);
+export default Node;
---
+++ test/meteoJS/modelviewer/node.test.js
@@ -0,0 +1,81 @@
+﻿const assert = require("assert");
+import Resource
+  from '../../../src/meteoJS/modelviewer/Resource.js';
+import VariableCollection
+  from '../../../src/meteoJS/modelviewer/VariableCollection.js';
+import Node from '../../../src/meteoJS/modelviewer/Node.js';
+import { Node as NodeClass } from '../../../src/meteoJS/modelviewer/Node.js';
+
+describe('Default Node, import via default', () => {
+  describe('children', () => {
+    it('simple append', () => {
+      let appendCounter = 0;
+      let node1 = new Node(new VariableCollection({ id: '1' }));
+      node1.on('append:child', child => {
+        appendCounter++;
+      });
+      let node2 = new Node(new VariableCollection({ id: '2' }));
+      let node3 = new Node(new VariableCollection({ id: '3' }));
+      node1.appendChild(node2).appendChild(node3);
+      assert.equal(node1.children.length, 2, 'node1 has 2 children');
+      assert.equal(appendCounter, 2, 'two append events');
+      assert.ok(node1.children[0] === node2, 'first child is node2');
+      assert.ok(node1.children[1] === node3, 'last child is node3');
+      assert.equal(node2.parents.length, 1, 'node2 has one parent');
+      assert.ok(node2.parents[0] === node1, 'node2\'s parent is node1');
+      assert.equal(node3.parents.length, 1, 'node3 has one parent');
+      assert.ok(node3.parents[0] === node1, 'node3\'s parent is node1');
+    });
+    it('multi append', () => {
+      let appendCounter = 0;
+      let node1 = new Node(new VariableCollection({ id: '1' }));
+      node1.on('append:child', child => {
+        appendCounter++;
+      });
+      let node2 = new Node(new VariableCollection({ id: '2' }));
+      let node3 = new Node(new VariableCollection({ id: '3' }));
+      node1.appendChild(node2, node3);
+      assert.equal(node1.children.length, 2, 'node1 has 2 children');
+      assert.equal(appendCounter, 2, 'two append events');
+      assert.ok(node1.children[0] === node2, 'first child is node2');
+      assert.ok(node1.children[1] === node3, 'last child is node3');
+      assert.equal(node2.parents.length, 1, 'node2 has one parent');
+      assert.ok(node2.parents[0] === node1, 'node2\'s parent is node1');
+      assert.equal(node3.parents.length, 1, 'node3 has one parent');
+      assert.ok(node3.parents[0] === node1, 'node3\'s parent is node1');
+    });
+  });
+  it('parents', () => {
+    let node1 = new Node(new VariableCollection({ id: '1' }));
+    let node2 = new Node(new VariableCollection({ id: '2' }));
+    let node3 = new Node(new VariableCollection({ id: '3' }));
+    node1._addParent(node2);
+    assert.equal(node1.parents.length, 1, 'node1 has one parent');
+    node1._addParent(node3);
+    assert.equal(node1.parents.length, 2, 'node1 has two parents');
+    assert.ok(node1.parents[0] === node2, 'first parent is node2');
+    assert.ok(node1.parents[1] === node3, 'last parent is node3');
+  });
+  it('resources', () => {
+    let r1 = new Resource();
+    let r2 = new Resource();
+    let r3 = new Resource();
+    let r4 = new Resource();
+    let r5 = new Resource();
+    let node = new Node(new VariableCollection({ id: 'test' }));
+    node.append(r1);
+    node.append(r2);
+    assert.equal(node.resources.length, 2, '2 resources');
+    node.append(r3, r4, r5);
+    assert.equal(node.resources.length, 5, '5 resources');
+    node.remove(r2, r4);
+    assert.equal(node.resources.length, 3, '3 resources');
+  });
+});
+describe('Node class, import via name', () => {
+  it('constructor test', () => {
+    let vc = new VariableCollection();
+    let node = new NodeClass(vc);
+    assert.ok(node.variableCollection === vc);
+  });
+});
