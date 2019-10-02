b5182196bee0f08b0931544c07a516191d4ab4ce
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-10-02 11:38:16 +0200

    Added class OffsetVariable (with tests)

---
+++ src/meteoJS/modelviewer/OffsetVariable.js
@@ -0,0 +1,87 @@
+/**
+ * @module meteoJS/modelviewer/offsetVariable
+ */
+import TimeVariable from './TimeVariable.js';
+
+/**
+ * Options for OffsetVariable constructor
+ * 
+ * @typedef {module:meteoJS/base/timeVariable~options}
+ *   module:meteoJS/modelviewer/offsetVariable~options
+ * @param {undefined|Date} [run] - Runtime.
+ * @param {undefined|integer} [offset] - Offset in seconds.
+ */
+
+/**
+ * @classdesc Class for something representing a time. The constructor supports
+ *   passing a runtime and an offset from this runtime.
+ */
+export class OffsetVariable extends TimeVariable {
+  
+  /**
+   * @param {module:meteoJS/modelviewer/offsetVariable~options} options - Options.
+   */
+  constructor({ id,
+                name = undefined,
+                names = {},
+                langSortation = [],
+                datetime = undefined,
+                run = undefined,
+                offset = undefined } = {}) {
+    super({
+      id,
+      name,
+      names,
+      langSortation,
+      datetime
+    });
+    
+    /**
+     * @type undefined|Date
+     */
+    this._run = run;
+    /**
+     * @type undefined|integer
+     */
+    this._offset = offset;
+    
+    this._updateDatetime();
+  }
+  
+  /**
+   * Runtime.
+   * 
+   * @type Date|undefined
+   */
+  get run() {
+    return this._run
+  }
+  set run(run) {
+    this._run = run;
+    this._updateDatetime();
+  }
+  
+  /**
+   * Offset.
+   * 
+   * @type integer|undefined
+   */
+  get offset() {
+    return this._offset
+  }
+  set offset(offset) {
+    this._offset = offset;
+    this._updateDatetime();
+  }
+  
+  /**
+   * Sets datetime according to run and offset.
+   * @private
+   */
+  _updateDatetime() {
+    if (this._run !== undefined &&
+        this._offset !== undefined)
+      this.datetime = new Date(this._run.valueOf() + this._offset * 1000);
+  }
+}
+export default OffsetVariable;
---
+++ test/meteoJS/modelviewer/offsetvariable.test.js
@@ -0,0 +1,123 @@
+﻿const assert = require("assert");
+import OffsetVariable from '../../../src/meteoJS/modelviewer/OffsetVariable.js';
+import { OffsetVariable as OffsetVariableClass }
+  from '../../../src/meteoJS/modelviewer/OffsetVariable.js';
+
+describe('OffsetVariable class, import via default', () => {
+  let d1 = new Date('2019-10-02T00:00:00');
+  let d2 = new Date('2018-01-01T00:00:00');
+  let d3 = new Date('2019-10-03T00:00:00');
+  describe('empty constructor', () => {
+    it('empty id/datetime', () => {
+      let offset = new OffsetVariable();
+      assert.equal(offset.id, undefined, 'id');
+      assert.equal(offset.datetime, undefined, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+    it('set datetime', () => {
+      let offset = new OffsetVariable();
+      offset.datetime = d1;
+      assert.equal(offset.id, d1.valueOf(), 'id');
+      assert.equal(offset.datetime, d1, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+    it('set id with valueOf(), empty again', () => {
+      let offset = new OffsetVariable();
+      offset.id = d2.valueOf();
+      assert.equal(offset.id, d2.valueOf(), 'id');
+      assert.equal(offset.datetime.valueOf(), d2.valueOf(), 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+      offset.datetime = undefined;
+      assert.equal(offset.id, undefined, 'id');
+      assert.equal(offset.datetime, undefined, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+  });
+  describe('constructor with datetime', () => {
+    it('construction', () => {
+      let offset = new OffsetVariable({
+        datetime: d1
+      });
+      assert.equal(offset.id, d1.valueOf(), 'id');
+      assert.equal(offset.datetime, d1, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+  });
+  describe('constructor with datetime and id', () => {
+    it('construction', () => {
+      let offset = new OffsetVariable({
+        id: d1.valueOf(),
+        datetime: d2
+      });
+      assert.equal(offset.id, d2.valueOf(), 'id');
+      assert.equal(offset.datetime, d2, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+  });
+  describe('constructor with id', () => {
+    it('construction', () => {
+      let offset = new OffsetVariable({
+        id: d1.valueOf()
+      });
+      assert.equal(offset.id, d1.valueOf(), 'id');
+      assert.equal(offset.datetime.valueOf(), d1.valueOf(), 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+    });
+  });
+  describe('constructor with run/offset', () => {
+    it('run/offset', () => {
+      let offset = new OffsetVariable({
+        run: d1,
+        offset: 24*3600
+      });
+      assert.equal(offset.id, d3.valueOf(), 'id');
+      assert.equal(offset.datetime.valueOf(), d3.valueOf(), 'datetime');
+      assert.equal(offset.run, d1, 'run');
+      assert.equal(offset.offset, 86400, 'offset');
+    });
+    it('run', () => {
+      let offset = new OffsetVariable({
+        run: d1
+      });
+      assert.equal(offset.id, undefined, 'id');
+      assert.equal(offset.datetime, undefined, 'datetime');
+      assert.equal(offset.run, d1, 'run');
+      assert.equal(offset.offset, undefined, 'offset');
+      offset.offset = 24*3600;
+      assert.equal(offset.id, d3.valueOf(), 'id');
+      assert.equal(offset.datetime.valueOf(), d3.valueOf(), 'datetime');
+      assert.equal(offset.run, d1, 'run');
+      assert.equal(offset.offset, 86400, 'offset');
+    });
+    it('offset', () => {
+      let offset = new OffsetVariable({
+        offset: 24*3600
+      });
+      assert.equal(offset.id, undefined, 'id');
+      assert.equal(offset.datetime, undefined, 'datetime');
+      assert.equal(offset.run, undefined, 'run');
+      assert.equal(offset.offset, 86400, 'offset');
+      offset.run = d1;
+      assert.equal(offset.id, d3.valueOf(), 'id');
+      assert.equal(offset.datetime.valueOf(), d3.valueOf(), 'datetime');
+      assert.equal(offset.run, d1, 'run');
+      assert.equal(offset.offset, 86400, 'offset');
+    });
+  });
+});
+describe('OffsetVariable class, import via name', () => {
+  it('simple', () => {
+    let offset = new OffsetVariableClass({
+      run: new Date('2019-10-02T10:00:00'),
+      offset: 24*3600
+    });
+    assert.equal(offset.datetime.valueOf(), (new Date('2019-10-03T10:00:00')).valueOf(), 'datetime');
+  });
+});
