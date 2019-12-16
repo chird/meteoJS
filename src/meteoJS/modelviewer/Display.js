03d75ba35a57bc74ba559d96f927efe4b896e9ff
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2019-12-16 14:58:51 +0100

    modelviewer/Display: further work * Added events

--- src/meteoJS/modelviewer/Display.js
+++ src/meteoJS/modelviewer/Display.js
@@ -2,10 +2,66 @@
  * @module meteoJS/modelviewer/display
  */
 import $ from 'jquery';
+import addEventFunctions from '../Events.js';
+
+/**
+ * Display initalisation event.
+ * 
+ * @event module:meteoJS/modelviewer/display#init:display
+ */
+
+/**
+ * VariableCollection add event. Also fired, once the object is assigned to
+ * a Container object.
+ * 
+ * @event module:meteoJS/modelviewer/display#add:variableCollection
+ * @type {Object}
+ * @property {module:meteoJS/modelviewer/variableCollection.VariableCollection}
+ *   variableCollection - The added variableCollection.
+ */
+
+/**
+ * Adding a variable to a variableCollection event. Also fired for each variable
+ * in the available variableCollections, once the object is assigned to a
+ * Container object.
+ * 
+ * @event module:meteoJS/modelviewer/display#add:variable
+ * @type {Object}
+ * @property {module:meteoJS/modelviewer/variable.Variable} variable
+ *   The added variable.
+ */
+
+/**
+ * Change available variables event.
+ * 
+ * @event module:meteoJS/modelviewer/display#change:availableVariables
+ * @type {Object}
+ * @property {Set<module:meteoJS/modelviewer/variable.Variable>}
+ *   availableVariables - Set of the available variables of a collection.
+ * @property {module:meteoJS/modelviewer/variableCollection.VariableCollection}
+ *   variableCollection - The collection all the variables belong to.
+ */
+
+/**
+ * Change selected variable event.
+ * 
+ * @event module:meteoJS/modelviewer/display#change:selectedVariable
+ * @type {Object}
+ * @property {module:meteoJS/modelviewer/variable.Variable} variable
+ *   The currently selected variable of the variableCollection. Could also be a
+ *   empty variable with no id.
+ * @property {module:meteoJS/modelviewer/variableCollection.VariableCollection}
+ *   variableCollection - VariableCollection.
+ */
 
 /**
  * @classdesc
- * @abstract
+ * 
+ * @fires module:meteoJS/modelviewer/display#init:display
+ * @fires module:meteoJS/modelviewer/display#add:variableCollection
+ * @fires module:meteoJS/modelviewer/display#add:variable
+ * @fires module:meteoJS/modelviewer/display#change:availableVariables
+ * @fires module:meteoJS/modelviewer/display#change:selectedVariable
  */
 export class Display {
   
@@ -24,7 +80,7 @@ export class Display {
     this._container = undefined;
     
     /**
-     * @type undefined|HTMLElement
+     * @type undefined|HTMLElement|jQuery
      * @private
      */
     this._parentNode = undefined;
@@ -39,10 +95,16 @@ export class Display {
   }
   set modelviewer(modelviewer) {
     this._modelviewer = modelviewer;
-    if (this._modelviewer !== undefined)
-      this._modelviewer.resources.variableCollections.forEach(collection => {
-        collection.on('add:variable', v => this.onAppendVariable(v));
+    if (this._modelviewer === undefined)
+      return;
+    
+    this._modelviewer.resources.variableCollections.forEach(collection => {
+      collection.on('add:variable', variable => {
+        this.trigger('add:variable', { variable })
       });
+    });
+    this._modelviewer.resources
+    .on('change:resources', () => this._changeResources());
   }
   
   /**
@@ -54,12 +116,19 @@ export class Display {
   }
   set container(container) {
     this._container = container;
-    if (this._container !== undefined)
-      this._container.on('change:visibleResource', e => this.onChangeVisibleResource(e));
+    if (this._container === undefined)
+      return;
+    
+    this._container.on('change:selectedVariables',
+    ({ addedVariables, removedVariables }) => {
+      this._onChangeSelectedVariables(addedVariables);
+    });
+    this._onChangeSelectedVariables();
+    this._container.on('change:visibleResource', e => this.onChangeVisibleResource(e));
   }
   
   /**
-   * @type HTMLElement
+   * @type HTMLElement|jQuery
    * @package
    */
   get parentNode() {
@@ -79,11 +148,24 @@ export class Display {
   }
   
   /**
-   * @abstract
-   * @protected
+   * @private
    */
   onInit() {
-    $(this.parentNode).empty();
+    if (this.parentNode === undefined)
+      return
+    
+    $(this.parentNode).empty()
+    this.trigger('init:display');
+    if (this._modelviewer === undefined)
+      return;
+    
+    this._modelviewer.resources.variableCollections
+    .forEach(collection => {
+      this.trigger('add:variableCollection', collection);
+      for (let variable of collection)
+        this.trigger('add:variable', variable);
+    });
+    this._onChangeSelectedVariables();
   }
   
   /**
@@ -96,9 +178,76 @@ export class Display {
   onChangeVisibleResource({ variable } = {}) {}
   
   /**
-   * @abstract
-   * @protected
+   * @private
    */
-  onAppendVariable(variable) {}
+  _onChangeSelectedVariables(addedVariables = undefined) {
+    this._changeResources();
+    
+    let selectedVariables = (addedVariables === undefined)
+    ? this._container.selectedVariables
+    : addedVariables;
+    this._modelviewer.resources.variableCollections
+    .forEach(variableCollection => {
+      [...selectedVariables].forEach(variable => {
+        if (variableCollection.contains(variable))
+          this.trigger('change:selectedVariable',
+                       { variable, variableCollection });
+      });
+    });
+  }
+  
+  /**
+   * @private
+   */
+  _changeResources() {
+    if (this._modelviewer === undefined ||
+        this._container === undefined)
+      return;
+    
+    for (let variableCollection of this._modelviewer.resources.variableCollections) {
+      let availableVariables = undefined;
+      if (variableCollection === this._modelviewer.resources.topNode.variableCollection ||
+          this._ignoreAvailableVariablesOfCollection.has(variableCollection))
+        availableVariables = new Set(variableCollection.variables);
+      else {
+        let selectedVariables = this._container.selectedVariables;
+   /*     if (selectedVariables.size == 0) {
+          let lastSelectedVariable;
+          [selectedVariables, lastSelectedVariable] =
+      this._container._getSelectedVariablesWithResources(
+        [this._modelviewer.resources.topNode],
+        new Set(),
+        undefined,
+        (selectedVariables, lastSelectedVariable) => {
+          let result = true;
+          this._modelviewer.resources._timesVariableCollections.forEach(collection => {
+            let contained = false;
+            for (let selectedVariable of selectedVariables) {
+              if (collection.contains(selectedVariable))
+                contained = true;
+            }
+            if (!contained)
+              result = false;
+          });
+          return result;
+        }
+      );
+          
+          if (selectedVariables === undefined)
+            selectedVariables = new Set();
+        }*/
+        
+        availableVariables =
+          this._modelviewer.resources
+          .getAvailableVariables(
+            variableCollection,
+            { variables: [...selectedVariables] }
+          );
+      }
+      
+      this.trigger('change:availableVariables', { availableVariables, variableCollection });
+    }
+  }
 }
+addEventFunctions(Display.prototype);
 export default Display;
