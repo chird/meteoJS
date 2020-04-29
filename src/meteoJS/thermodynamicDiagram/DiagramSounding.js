d137e91242816bccadd3477656c3cafc677e3684
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2020-04-29 13:36:32 +0200

    thermodynamicDiagram.DiagramSounding: API changes
    * Methods/properties/events/tests added

--- src/meteoJS/thermodynamicDiagram/DiagramSounding.js
+++ src/meteoJS/thermodynamicDiagram/DiagramSounding.js
@@ -1,147 +1,294 @@
 /**
  * @module meteoJS/thermodynamicDiagram/sounding
  */
-import $ from 'jquery';
 import addEventFunctions from '../Events.js';
 
 /**
+ * Change visibility event. Only triggered, if the visibility of the sounding
+ * changes, not if only a part's visibility (like hodograph) changes.
+ * 
  * @event module:meteoJS/thermodynamicDiagram/sounding#change:visible
  */
 
 /**
- * Definition of the options for the constructor.
- * @typedef {Object} module:meteoJS/thermodynamicDiagram/sounding~options
- * @param {boolean} visible Visibility of sounding
- * @param {Object} diagram Options for thermodynamic diagram
- * @param {boolean} diagram.visible
- *   Visibility in thermodynamic diagram of this sounding
- * @param {Object} diagram.temp Options for temperature curve
- * @param {boolean} diagram.temp.visible
- *   Visibility of temperature curve in thermodynamic diagram
- * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions} diagram.temp.style
- *   Style for temperature curve
- * @param {Object} diagram.dewp Options for dewpoint temperature curve
- * @param {boolean} diagram.dewp.visible
- *   Visibility of dewpoint temperature curve in thermodynamic diagram
- * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions} diagram.dewp.style
- *   Style for dewpoint temperature curve
- * @param {module:meteoJS/thermodynamicDiagram/windprofile~options}
- *   windprofile
- *   Windprofile options.
- * @param {Object} hodograph Options for hodograph
- * @param {boolean} hodograph.visible
- *   Visibility in hodograph of this sounding
+ * Change options event.
+ * 
+ * @event module:meteoJS/thermodynamicDiagram/sounding#change:options
  */
 
 /**
- * Representation of a plottet sounding (data and display options)
+ * A line with its visibility and style.
+ * 
+ * @typedef {Object} module:meteoJS/thermodynamicDiagram~lineOptions
+ * @param {boolean} [visible=true] - Visibility of the line.
+ * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
+ *   [style] - Line style.
+ */
+
+/**
+ * Definition of the options for the constructor.
+ * 
+ * @typedef {Object} module:meteoJS/thermodynamicDiagram/sounding~options
+ * @param {boolean} [visible=true] - Visibility of the sounding.
+ * @param {Object} [diagram] - Options for the thermodynamic diagram part.
+ * @param {boolean} [diagram.visible=true]
+ *   Visibility of this sounding in the thermodynamic diagram.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [diagram.temp] - Options for the temperature curve of this sounding.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [diagram.dewp] - Options for the dewpoint curve of this sounding.
+ * @param {Object} [windprofile] - Options for the windprofile part.
+ * @param {boolean} [windprofile.visible=true]
+ *   Visibility of this sounding in the windprofile part.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [windprofile.windbarbs] - Options for the windbarbs.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [windprofile.windspeed] - Options for the windspeed line.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [hodograph] - Options for this sounding for the hodograph.
+ */
+
+/**
+ * Representation of a plotted sounding (data and display options)
+ * 
+ * @fires module:meteoJS/thermodynamicDiagram/sounding#change:visible
+ * @fires module:meteoJS/thermodynamicDiagram/sounding#change:options
  */
 export class DiagramSounding {
   
   /**
    * @param {module:meteoJS/sounding.Sounding} sounding - Sounding data.
-   * @param {module:meteoJS/thermodynamicDiagram/sounding~options} options - Options.
+   * @param {module:meteoJS/thermodynamicDiagram/sounding~options} [options] - Options.
    */
-  constructor(sounding, options) {
+  constructor(sounding, {
+    visible = true,
+    diagram = {},
+    windprofile = {},
+    hodograph = {}
+  } = {}) {
     /**
      * @type module:meteoJS/sounding.Sounding
      * @private
      */
-    this.sounding = sounding;
-  
+    this._sounding = sounding;
+    
     /**
-     * @type module:meteoJS/thermodynamicDiagram/sounding~options
+     * @type boolean
      * @private
      */
-    this.options = $.extend(true, {
-      visible: true,
-      diagram: {
-        visible: true,
-        temp: {
-          visible: true,
-          style: {
-            color: undefined,
-            width: 1,
-            opacity: undefined,
-            linecap: undefined,
-            linejoin: undefined,
-            dasharray: undefined
-          }
-        },
-        dewp: {
-          visible: true,
-          style: {
-            color: undefined,
-            width: 1,
-            opacity: undefined,
-            linecap: undefined,
-            linejoin: undefined,
-            dasharray: undefined
-          }
-        }
-      },
-      windprofile: {
-        visible: true,
-        windbarbs: {
-          visible: true,
-          style: {
-            color: undefined,
-            width: 1,
-            opacity: undefined,
-            linecap: undefined,
-            linejoin: undefined,
-            dasharray: undefined
-          }
-        },
-        windspeed: {
-          visible: true,
-          style: {
-            color: undefined,
-            width: 1,
-            opacity: undefined,
-            linecap: undefined,
-            linejoin: undefined,
-            dasharray: undefined
-          }
-        }
-      },
-      hodograph: {
-        visible: true,
-        style: {
-          width: 1
-        }
-      }
-    }, options);
+    this._visible = visible;
+    
+    /**
+     * @type Object
+     * @private
+     */
+    this._options = {
+      diagram: normalizeDiagramOptions(diagram),
+      windprofile:  normalizeWindprofileOptions(windprofile),
+      hodograph: normalizeVisibilityAndStyleOptions(hodograph)
+    };
   }
 
   /**
-   * Returns sounding data.
+   * Sounding data.
    * 
-   * @returns {module:meteoJS/sounding.Sounding} Sounding data.
+   * @type module:meteoJS/sounding.Sounding
+   * @readonly
    */
-  getSounding() {
-    return this.sounding;
+  get sounding() {
+    return this._sounding;
   }
-
+  
   /**
-   * Visibility of sounding in the diagram.
+   * Visibility of the sounding.
    * 
-   * @param {boolean} [visible] Visibility.
-   * @returns {boolean|module:meteoJS/thermodynamicDiagram/sounding.DiagramSounding} Either visibility or this.
-   * @fires {module:meteoJS/thermodynamicDiagram/sounding#change:visible}
+   * @type {boolean}
+   * @fires module:meteoJS/thermodynamicDiagram/sounding#change:visible
    */
-  visible(visible) {
-    if (visible === undefined)
-      return this.options.visible;
-    else {
-      let old = this.options.visible;
-      this.options.visible = visible ? true : false;
-      if (old != this.options.visible)
-        this.trigger('change:visible', this);
-      return this;
-    }
+  get visible() {
+    return this._visible;
   }
-
+  set visible(visible) {
+    let oldVisible = this._visible;
+    this._visible = visible ? true : false;
+    if (oldVisible != this._visible)
+      this.trigger('change:visible');
+  }
+  
+  get options() {
+    return this._options;
+  }
+  
+  /**
+   * Updated the style options for this sounding.
+   * 
+   * @param {module:meteoJS/thermodynamicDiagram/sounding~options}
+   *   [options] - Options.
+   * @fires module:meteoJS/thermodynamicDiagram/sounding#change:visible
+   * @fires module:meteoJS/thermodynamicDiagram/sounding#change:options
+   */
+  update({
+    visible = undefined,
+    diagram = undefined,
+    windprofile = undefined,
+    hodograph = undefined
+  } = {}) {
+    let willTrigger = false;
+    if (diagram === undefined)
+      diagram = {};
+    else
+      willTrigger = true;
+    if (windprofile === undefined)
+      windprofile = {};
+    else
+      willTrigger = true;
+    if (hodograph === undefined)
+      hodograph = {};
+    else
+      willTrigger = true;
+    
+    if ('visible' in diagram)
+      this._options.diagram.visible = diagram.visible;
+    ['temp', 'dewp'].forEach(key => {
+      if (key in diagram)
+        this._options.diagram[key] =
+          updateLineOptions(this._options.diagram[key], diagram[key]);
+    });
+    if ('visible' in windprofile)
+      this._options.windprofile.visible = windprofile.visible;
+    ['windbarbs', 'windspeed'].forEach(key => {
+      if (key in windprofile)
+        this._options.windprofile[key] =
+          updateLineOptions(this._options.windprofile[key], windprofile[key]);
+    });
+    this._options.hodograph =
+      updateLineOptions(this._options.hodograph, hodograph);
+    if (willTrigger)
+      this.trigger('change:options');
+    
+    if (visible !== undefined)
+      this.visible = visible;
+  }
 }
 addEventFunctions(DiagramSounding.prototype);
 export default DiagramSounding;
+
+/**
+ * Normalizes DiagramSounding-Options for the diagram part.
+ * 
+ * @param {Object} [options] - Options.
+ * @param {boolean} [options.visible=true]
+ *   Visibility of this sounding in the thermodynamic diagram.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [options.temp] - Options for the temperature curve of this sounding.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [options.dewp] - Options for the dewpoint curve of this sounding.
+ * @returns {Object} - Normalized options.
+ * @private
+ */
+function normalizeDiagramOptions({
+  visible = true,
+  temp = {},
+  dewp = {}
+} = {}) {
+  return {
+    visible,
+    temp: normalizeVisibilityAndStyleOptions(temp),
+    dewp: normalizeVisibilityAndStyleOptions(dewp)
+  };
+}
+
+/**
+ * Normalizes DiagramSounding-Options for the windprofile part.
+ * 
+ * @param {Object} [options] - Options.
+ * @param {boolean} [options.visible=true]
+ *   Visibility of this sounding in the windprofile part.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [options.windbarbs] - Options for the windbarbs.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [options.windspeed] - Options for the windspeed line.
+ * @returns {Object} - Normalized options.
+ * @private
+ */
+function normalizeWindprofileOptions({
+  visible = true,
+  windbarbs = {},
+  windspeed = {}
+} = {}) {
+  return {
+    visible,
+    windbarbs: normalizeVisibilityAndStyleOptions(windbarbs),
+    windspeed: normalizeVisibilityAndStyleOptions(windspeed)
+  };
+}
+
+/**
+ * Normalizes DiagramSounding-Options with visibility and style.
+ * 
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [options] - Options.
+ * @returns {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   Normalized options.
+ * @private
+ */
+function normalizeVisibilityAndStyleOptions({
+  visible = true,
+  style = {}
+} = {}) {
+  return {
+    visible,
+    style: normalizeLineStyleOptions(style)
+  };
+}
+
+/**
+ * Normalizes lineStyle-Options.
+ * 
+ * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
+ *   [options] - Options.
+ * @returns {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
+ *   Normalized options.
+ * @private
+ */
+function normalizeLineStyleOptions({
+  color = undefined,
+  width = 1,
+  opacity = undefined,
+  linecap = undefined,
+  linejoin = undefined,
+  dasharray = undefined
+} = {}) {
+  return {
+    color,
+    width,
+    opacity,
+    linecap,
+    linejoin,
+    dasharray
+  };
+}
+
+/**
+ * Updates DiagramSounding-Options with visibility and style.
+ * 
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   options - Current options.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   updateOptions - Some new options.
+ * @returns {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   New options object.
+ * @private
+ */
+function updateLineOptions(options, updateOptions) {
+  if ('visible' in updateOptions)
+    options.visible = updateOptions.visible;
+  if ('style' in updateOptions) {
+    ['color', 'width', 'opacity',  'linecap',  'linejoin',  'dasharray']
+      .forEach(styleKey => {
+        if (styleKey in updateOptions.style)
+          options.style[styleKey] = updateOptions.style[styleKey];
+      });
+  }
+  return options;
+}
---
+++ test/meteoJS/thermodynamicDiagram/diagramSounding.test.js
@@ -0,0 +1,207 @@
+﻿import assert from 'assert';
+import Sounding  from '../../../src/meteoJS/Sounding.js';
+import { default as DiagramSounding, DiagramSounding as DiagramSoundingClass }
+  from '../../../src/meteoJS/thermodynamicDiagram/DiagramSounding.js';
+
+describe('DiagramSounding class, import via default', () => {
+  it('Empty constructor', () => {
+    let s = new DiagramSounding();
+    assert.ok(s.sounding === undefined, 'sounding');
+    assert.ok(s.visible, 'visible');
+    let changeVisibleCounter = 0;
+    s.on('change:visible', () => changeVisibleCounter++);
+    s.visible = false;
+    assert.ok(!s.visible, 'visible');
+    s.visible = true;
+    assert.ok(s.visible, 'visible');
+    assert.equal(changeVisibleCounter, 2, 'changeVisibleCounter');
+  });
+  it('Constructor with Sounding', () => {
+    let sounding = new Sounding();
+    let s = new DiagramSounding(sounding);
+    assert.equal(s.sounding, sounding, 'sounding');
+  });
+  it('Default Options', () => {
+    let s = new DiagramSounding();
+    ['diagram', 'windprofile', 'hodograph'].forEach(key => {
+      assert.ok(key in s.options, key);
+      assert.ok('visible' in s.options[key], 'visible');
+      assert.ok(s.options[key].visible, 'visible default');
+    });
+    assert.ok('temp' in s.options.diagram, 'temp');
+    assert.ok('dewp' in s.options.diagram, 'dewp');
+    assert.ok('windbarbs' in s.options.windprofile, 'windbarbs');
+    assert.ok('windspeed' in s.options.windprofile, 'windspeed');
+    const lineOptions = new Map([
+      ['color', undefined],
+      ['width', 1],
+      ['opacity', undefined],
+      ['linecap', undefined],
+      ['linejoin', undefined],
+      ['dasharray', undefined],
+    ]);
+    [['diagram', ['temp', 'dewp']],
+     ['windprofile', ['windbarbs', 'windspeed']]].forEach(tests => {
+      tests[1].forEach(key => {
+        assert.ok(key in s.options[tests[0]], key);
+        assert.ok('visible' in s.options[tests[0]][key], 'visible');
+        assert.ok(s.options[tests[0]][key].visible, 'visible default');
+        assert.ok('style' in s.options[tests[0]][key], 'style');
+        for (let t of lineOptions) {
+          assert.ok(t[0] in s.options[tests[0]][key].style, t[0]);
+          assert.equal(s.options[tests[0]][key].style[t[0]], t[1], t[1]);
+        }
+      });
+    });
+    assert.ok('style' in s.options.hodograph, 'style');
+    for (let t of lineOptions) {
+      assert.ok(t[0] in s.options.hodograph.style, t[0]);
+      assert.equal(s.options.hodograph.style[t[0]], t[1], t[1]);
+    }
+  });
+  it('Partly changed Options', () => {
+    let s = new DiagramSounding(undefined, {
+      diagram: {
+        dewp: {
+          style: {
+            color: 'red'
+          }
+        }
+      },
+      windprofile: {
+        visible: false
+      },
+      hodograph: {
+        visible: true,
+        style: {
+          width: 2
+        }
+      }
+    });
+    ['diagram', 'windprofile', 'hodograph'].forEach(key => {
+      assert.ok(key in s.options, key);
+      assert.ok('visible' in s.options[key], 'visible');
+      assert.ok(s.options[key].visible == (key != 'windprofile'), 'visible default');
+    });
+    assert.ok('temp' in s.options.diagram, 'temp');
+    assert.ok('dewp' in s.options.diagram, 'dewp');
+    assert.ok('windbarbs' in s.options.windprofile, 'windbarbs');
+    assert.ok('windspeed' in s.options.windprofile, 'windspeed');
+    const lineOptions = new Map([
+      ['color', undefined],
+      ['width', 1],
+      ['opacity', undefined],
+      ['linecap', undefined],
+      ['linejoin', undefined],
+      ['dasharray', undefined],
+    ]);
+    [['diagram', ['temp', 'dewp']],
+     ['windprofile', ['windbarbs', 'windspeed']]].forEach(tests => {
+      tests[1].forEach(key => {
+        assert.ok(key in s.options[tests[0]], key);
+        assert.ok('visible' in s.options[tests[0]][key], 'visible');
+        assert.ok(s.options[tests[0]][key].visible, 'visible default');
+        assert.ok('style' in s.options[tests[0]][key], 'style');
+        for (let t of lineOptions) {
+          assert.ok(t[0] in s.options[tests[0]][key].style, t[0]);
+          if (tests[0] == 'diagram' && key == 'dewp' && t[0] == 'color')
+            assert.equal(s.options[tests[0]][key].style[t[0]], 'red', 'red');
+          else
+            assert.equal(s.options[tests[0]][key].style[t[0]], t[1], t[1]);
+        }
+      });
+    });
+    assert.ok('style' in s.options.hodograph, 'style');
+    for (let t of lineOptions) {
+      assert.ok(t[0] in s.options.hodograph.style, t[0]);
+      if (t[0] == 'width')
+        assert.equal(s.options.hodograph.style[t[0]], 2, 'width');
+      else
+        assert.equal(s.options.hodograph.style[t[0]], t[1], t[1]);
+    }
+  });
+  it('update()', () => {
+    let s = new DiagramSounding();
+    let changeOptionsCounter = 0;
+    s.on('change:options', () => changeOptionsCounter++);
+    let changeVisibleCounter = 0;
+    s.on('change:visible', () => changeVisibleCounter++);
+    assert.ok(s.visible, 'visible');
+    assert.ok(s.options.diagram.visible, 'visible');
+    assert.ok(s.options.windprofile.visible, 'visible');
+    assert.ok(s.options.hodograph.visible, 'visible');
+    s.update({
+      windprofile: {
+        visible: false
+      },
+      hodograph: {
+        visible: false
+      }
+    });
+    assert.ok(s.visible, 'visible');
+    assert.ok(s.options.diagram.visible, 'visible');
+    assert.ok(!s.options.windprofile.visible, 'visible');
+    assert.ok(!s.options.hodograph.visible, 'visible');
+    assert.equal(changeOptionsCounter, 1, 'changeOptionsCounter');
+    assert.equal(changeVisibleCounter, 0, 'changeVisibleCounter');
+    s.update({
+      visible: false
+    });
+    assert.ok(!s.visible, 'visible');
+    assert.ok(s.options.diagram.visible, 'visible');
+    assert.ok(!s.options.windprofile.visible, 'visible');
+    assert.ok(!s.options.hodograph.visible, 'visible');
+    assert.equal(changeOptionsCounter, 1, 'changeOptionsCounter');
+    assert.equal(changeVisibleCounter, 1, 'changeVisibleCounter');
+    s.update({
+      diagram: {
+        temp: {
+          style: { color: 'red' }
+        },
+        dewp: {
+          style: { color: 'blue' }
+        }
+      },
+      windprofile: {
+        visible: true,
+        windbarbs: {
+          style: { color: 'yellow' }
+        },
+        windspeed: {
+          style: { color: 'gray' }
+        }
+      },
+      hodograph: {
+        visible: true,
+        style: { color: 'green' }
+      }
+    });
+    assert.ok(!s.visible, 'visible');
+    assert.ok(s.options.diagram.visible, 'visible');
+    assert.ok(s.options.windprofile.visible, 'visible');
+    assert.ok(s.options.hodograph.visible, 'visible');
+    assert.equal(s.options.diagram.temp.style.color, 'red', 'color');
+    assert.equal(s.options.diagram.temp.style.width, '1', 'width');
+    assert.equal(s.options.diagram.dewp.style.color, 'blue', 'color');
+    assert.equal(s.options.diagram.dewp.style.width, '1', 'width');
+    assert.equal(s.options.windprofile.windbarbs.style.color, 'yellow', 'color');
+    assert.equal(s.options.windprofile.windbarbs.style.width, '1', 'width');
+    assert.equal(s.options.windprofile.windspeed.style.color, 'gray', 'color');
+    assert.equal(s.options.windprofile.windspeed.style.width, '1', 'width');
+    assert.equal(s.options.hodograph.style.color, 'green', 'color');
+    assert.equal(s.options.hodograph.style.width, '1', 'width');
+    assert.equal(changeOptionsCounter, 2, 'changeOptionsCounter');
+    assert.equal(changeVisibleCounter, 1, 'changeVisibleCounter');
+    s.visible = true;
+    assert.ok(s.visible, 'visible');
+    assert.equal(changeOptionsCounter, 2, 'changeOptionsCounter');
+    assert.equal(changeVisibleCounter, 2, 'changeVisibleCounter');
+  });
+});
+describe('DiagramSounding class, import via name', () => {
+  it('empty object', () => {
+    let s = new DiagramSoundingClass();
+    assert.ok(s.sounding === undefined, 'sounding');
+    assert.ok(s.visible, 'visible');
+  });
+});
