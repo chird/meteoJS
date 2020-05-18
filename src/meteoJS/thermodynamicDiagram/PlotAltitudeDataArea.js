bb25e64c2cd824d305ba06c2be072466489a3978
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2020-05-18 11:23:18 +0200

    Thermodynamic diagram: added class PlotAltitudeDataArea

---
+++ src/meteoJS/thermodynamicDiagram/PlotAltitudeDataArea.js
@@ -0,0 +1,177 @@
+/**
+ * @module meteoJS/thermodynamicDiagram/plotAltitudeDataArea
+ */
+import PlotDataArea from './plotDataArea.js';
+
+/**
+ * Function to insert labels.
+ * 
+ * @typedef {Function}
+ *   module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~insertLabelsFunc
+ * @param {module:meteoJS/thermodynamicDiagram/diagramSounding.DiagramSounding}
+ *   sounding - Diagram sounding to label.
+ * @param {Object} levelData - Data to label.
+ * @param {external:SVG} group - SVG group to insert labels.
+ */
+
+/**
+ * Options for labels on hovering the diagram.
+ * 
+ * @typedef {Object}
+ *   module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~hoverLabelsOptions
+ * @property {boolean} [visible=true] - Visibility.
+ * @property {string} [type='mousemove'] - Event type.
+ * @property {boolean} [snapToData=true]
+ *   Snap labels to data points.
+ * @property {boolean} [remote=true]
+ *   Show labels relative to the mouse position on the diagram, even when the
+ *   pointer isn't directly on the plot area.
+ * @property {module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~insertLabelsFunc}
+ *   [insertLabelsFunc] - Called to insert labels into a SVG group.
+ */
+
+/**
+ * Options for the constructor.
+ * 
+ * @typedef {module:meteoJS/thermodynamicDiagram/plotDataArea~options}
+ *   module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~options
+ * @param {module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~hoverLabelsOptions}
+ *   [hoverLabels] - Hover labels options.
+ */
+
+/**
+ * Abstract class to define an area on the SVG with sounding data, plotted with
+ * pressure on the y-axis.
+ * 
+ * @extends module:meteoJS/thermodynamicDiagram/plotDataArea.PlotDataArea
+ */
+export class PlotAltitudeDataArea extends PlotDataArea {
+  
+  /**
+   * @param {module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~options}
+   *   options - Options.
+   */
+  constructor({
+    svgNode,
+    coordinateSystem,
+    x = 0,
+    y = 0,
+    width = 100,
+    height = 100,
+    style = {},
+    visible = true,
+    events = {},
+    hoverLabels = {},
+    getSoundingVisibility = sounding => sounding.visible
+  }) {
+    super({
+      svgNode,
+      coordinateSystem,
+      x,
+      y,
+      width,
+      height,
+      style,
+      visible,
+      events,
+      getSoundingVisibility
+    });
+    
+    /**
+     * @type external:SVG
+     * @private
+     */
+    this._hoverLabelsGroup = this._svgNode.group();
+    
+    /**
+     * @type boolean
+     * @private
+     */
+    this._isHoverLabelsRemote = true;
+    
+    this._initHoverLabels(hoverLabels);
+  }
+  
+  /**
+   * Called, when a sounding changes its visibilty.
+   * 
+   * @override
+   */
+  onChangeSoundingVisibility(sounding, group) {
+    super.onChangeSoundingVisibility(sounding, group);
+    this._hoverLabelsGroup.clear();
+  }
+  
+  /**
+   * Draw the sounding into the SVG group.
+   * 
+   * @override
+   */
+  drawSounding(sounding, group) {
+    super.drawSounding(sounding, group);
+    this._hoverLabelsGroup.clear();
+  }
+  
+  /**
+   * Extend an event with pressure.
+   * 
+   * @override
+   */
+  getExtendedEvent(e, p) {
+    e = super.getExtendedEvent(e, p);
+    e.diagramPres =
+      this.coordinateSystem.getPByXY(0,
+        this.coordinateSystem.getHeight() - e.elementY);
+    return e;
+  }
+  
+  /**
+   * Show also hover labels when mouse isn't over the area.
+   * 
+   * @type boolean
+   * @readonly
+   */
+  get isHoverLabelsRemote() {
+    return this._isHoverLabelsRemote;
+  }
+  
+  get hoverLabelsSounding() {
+      // Wie "manuell" setzen?
+    for (let sounding of this._soundings.keys()) {
+      if (this._getSoundingVisibility(sounding))
+        return sounding;
+    }
+  }
+  
+  /**
+   * Initialize hover labels options.
+   * 
+   * @param {module:meteoJS/thermodynamicDiagram/plotAltitudeDataArea~hoverLabelsOptions}
+   *   options - Hover labels options.
+   */
+  _initHoverLabels({
+    visible = true,
+    type = 'mousemove',
+    snapToData = true,
+    remote = true,
+    insertLabelsFunc = undefined
+  }) {
+    this._isHoverLabelsRemote = remote;
+    
+    if (!visible ||
+        insertLabelsFunc === undefined)
+      return;
+    
+    this.on('change:extent', () => this._hoverLabelsGroup.clear());
+    this.on(type, e => {
+      if (!e.diagramPres)
+        return;
+      
+      const sounding = this.hoverLabelsSounding.sounding;
+      insertLabelsFunc(this.hoverLabelsSounding,
+        sounding.getData(sounding.getNearestLevel(e.diagramPres)),
+        this._hoverLabelsGroup);
+    });
+  }
+}
+export default PlotAltitudeDataArea;
---
+++ test/meteoJS/thermodynamicDiagram/plotAltitudeDataArea.test.js
@@ -0,0 +1,135 @@
+﻿import assert from 'assert';
+import { createSVGWindow } from 'svgdom';
+import { Event } from 'svgdom/src/dom/Event';
+import { SVG, registerWindow } from '@svgdotjs/svg.js';
+global.window = createSVGWindow();
+global.document = window.document;
+import SkewTlogPDiagram from '../../../src/meteoJS/thermodynamicDiagram/coordinateSystem/SkewTlogPDiagram.js';
+import Sounding from '../../../src/meteoJS/Sounding.js';
+import DiagramSounding from '../../../src/meteoJS/thermodynamicDiagram/DiagramSounding.js';
+import {
+  default as PlotAltitudeDataArea,
+  PlotAltitudeDataArea as PlotAltitudeDataAreaClass
+} from '../../../src/meteoJS/thermodynamicDiagram/PlotAltitudeDataArea.js';
+
+registerWindow(global.window, global.document);
+
+describe('PlotAltitudeDataArea class, import via default', () => {
+  it('hoverLabelsSounding', () => {
+    const svgNode = SVG().size(300,300);
+    const coordinateSystem = new SkewTlogPDiagram();
+    const plotArea = new PlotAltitudeDataArea({
+      svgNode,
+      coordinateSystem,
+      hoverLabels: {
+        remote: false
+      }
+    });
+    assert.ok(!plotArea.isHoverLabelsRemote, 'isHoverLabelsRemote');
+    const s1 = new DiagramSounding(new Sounding());
+    const s2 = new DiagramSounding(new Sounding());
+    const s3 = new DiagramSounding(new Sounding());
+    plotArea.addSounding(s1);
+    plotArea.addSounding(s2);
+    plotArea.addSounding(s3);
+    assert.equal(s1.visible, true, 's1 visible');
+    assert.equal(s2.visible, true, 's2 visible');
+    assert.equal(s3.visible, true, 's3 visible');
+    assert.equal(plotArea.hoverLabelsSounding, s1, 'hoverLabelsSounding');
+    s1.visible = false;
+    assert.equal(plotArea.hoverLabelsSounding, s2, 'hoverLabelsSounding');
+    s2.visible = false;
+    assert.equal(plotArea.hoverLabelsSounding, s3, 'hoverLabelsSounding');
+  });
+  it('mousemove-Event', () => {
+    const sounding = new Sounding();
+    for (let pres=1000; pres>=100; pres-=50) {
+      const tmpk = 73.15 + (Math.floor(Math.random() * 4000) - 2000) / 100;
+      sounding.addLevel({
+        pres,
+        tmpk,
+        dwpk: tmpk - Math.floor(Math.random() * 3000)/100
+      });
+    }
+    const s = new DiagramSounding(sounding);
+    const mousemoveEvent = new Event('mousemove');
+    mousemoveEvent.pageX = 100;
+    mousemoveEvent.pageY = 100;
+    const mousemoveRemoteEvent = new Event('mousemove');
+    mousemoveRemoteEvent.pageX = 5;
+    mousemoveRemoteEvent.pageY = 140;
+    
+    let insertFuncCounter = 0;
+    const svgNode = SVG().size(300,300);
+    const coordinateSystem = new SkewTlogPDiagram();
+    const plotArea = new PlotAltitudeDataArea({
+      svgNode,
+      coordinateSystem,
+      x: 50,
+      y: 50,
+      width: 200,
+      height: 200,
+      hoverLabels: {
+        insertLabelsFunc: (sounding, levelData, group) => {
+          group.clear();
+          assert.equal(sounding, s, 'sounding');
+          assert.ok((levelData.pres == 300) ? true : (levelData.pres == 800) ? true : false, 'pres');
+          assert.equal(group, plotArea._hoverLabelsGroup, '_hoverLabelsGroup');
+          group.circle(50);
+          insertFuncCounter++;
+        }
+      }
+    });
+    assert.ok(plotArea.isHoverLabelsRemote, 'isHoverLabelsRemote');
+    plotArea.addSounding(s);
+    assert.equal(insertFuncCounter, 0, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+    plotArea._svgNode.dispatchEvent(mousemoveEvent);
+    assert.equal(insertFuncCounter, 1, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 1, 'hoverLabelsGroup');
+    plotArea.width = 100;
+    plotArea.height = 100;
+    assert.equal(insertFuncCounter, 1, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+    plotArea._svgNode.dispatchEvent(mousemoveEvent);
+    assert.equal(insertFuncCounter, 2, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 1, 'hoverLabelsGroup');
+    s.visible = false;
+    assert.equal(insertFuncCounter, 2, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+    s.visible = true;
+    assert.equal(insertFuncCounter, 2, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+    plotArea._svgNode.dispatchEvent(mousemoveEvent);
+    assert.equal(insertFuncCounter, 3, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 1, 'hoverLabelsGroup');
+    s.update({
+      diagram: {
+        temp: {
+          style: {
+            color: 'red'
+          }
+        }
+      }
+    });
+    assert.equal(insertFuncCounter, 3, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+    plotArea._svgNode.dispatchEvent(mousemoveEvent);
+    assert.equal(insertFuncCounter, 4, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 1, 'hoverLabelsGroup');
+    plotArea._svgNode.dispatchEvent(mousemoveRemoteEvent);
+    assert.equal(insertFuncCounter, 5, 'insertFuncCounter');
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 1, 'hoverLabelsGroup');
+  });
+});
+describe('PlotAltitudeDataArea class, import via name', () => {
+  it('simple test', () => {
+    const svgNode = SVG().size(300,300);
+    const coordinateSystem = new SkewTlogPDiagram();
+    const plotArea = new PlotAltitudeDataAreaClass({
+      svgNode,
+      coordinateSystem
+    });
+    assert.equal(plotArea._hoverLabelsGroup.children().length, 0, 'hoverLabelsGroup');
+  });
+});
