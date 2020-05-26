6f8743a700fda37291968ec0480d520390d1b86a
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2020-05-26 13:56:36 +0200

    Thermodynamic diagram: functions adjusted and some tests added

--- src/meteoJS/thermodynamicDiagram/Functions.js
+++ src/meteoJS/thermodynamicDiagram/Functions.js
@@ -3,15 +3,17 @@
  */
 
 /**
- * Definition of a line style.
+ * Definition of a line style. Some properties misses.
  * 
  * @typedef {Object} module:meteoJS/thermodynamicDiagram~lineStyleOptions
- * @param {string} [color='black'] - Color.
- * @param {number} [width=1] - Width.
- * @param {undefined|number} [opacity=undefined] - Opacity.
- * @param {} [linecap=undefined] - Linecap.
- * @param {} [linejoin=undefined] - Linejoin.
- * @param {} [dasharray=undefined] - Dasharray.
+ * @property {string} [color='black'] - Color.
+ * @property {number} [width=1] - Width.
+ * @property {undefined|number} [opacity=undefined] - Opacity.
+ * @property {'butt'|'round'|'square'} [linecap=undefined] - Linecap.
+ * @property {'arcs'|'bevel'|'miter'|'miter-clip'|'round'} [linejoin=undefined]
+ *   Linejoin.
+ * @property {string} [dasharray=undefined] - Dasharray.
+ * @property {number|string} [dashoffset=undefined] - Dashoffset.
  */
 
 /**
@@ -19,26 +21,59 @@
  * 
  * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
  *   [options] - Options.
+ * @param {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
+ *   [defaults] - Optional defaults.
  * @returns {module:meteoJS/thermodynamicDiagram~lineStyleOptions}
  *   Normalized options.
  * @private
  */
 export function getNormalizedLineStyleOptions({
-  color = 'black',
-  width = 1,
-  opacity = undefined,
-  linecap = undefined,
-  linejoin = undefined,
-  dasharray = undefined
-} = {}) {
-  return {
-    color,
-    width,
-    opacity,
-    linecap,
-    linejoin,
-    dasharray
-  };
+  color = undefined,
+  width = undefined,
+  ...result
+} = {}, defaults = {}) {
+  result.color = getFirstDefinedValue(color, defaults.color, 'black');
+  result.width = getFirstDefinedValue(width, defaults.width, 1);
+  return result;
+}
+
+/**
+ * Definition of font options. Some properties misses.
+ * 
+ * @typedef {Object} module:meteoJS/thermodynamicDiagram~fontOptions
+ * @param {mixed} [size=12] - Size.
+ * @param {mixed} [color='black'] - Color.
+ * @param {'start'|'middle'|'end'} [anchor=undefined] - Anchor.
+ */
+
+/**
+ * Returns normalized font-Options.
+ * 
+ * @param {module:meteoJS/thermodynamicDiagram~fontOptions}
+ *   [options] - Options.
+ * @param {module:meteoJS/thermodynamicDiagram~fontOptions}
+ *   [defaults] - Optional defaults.
+ * @returns {module:meteoJS/thermodynamicDiagram~fontOptions}
+ *   Normalized options.
+ * @private
+ */
+export function getNormalizedFontOptions({
+  size = undefined,
+  color = undefined,
+  anchor = undefined,
+  ...result
+} = {}, defaults = {}) {
+  result.size = getFirstDefinedValue(size, defaults.size, 12);
+  result.color = getFirstDefinedValue(color, defaults.color, 'black');
+  anchor = getFirstDefinedValue(anchor, defaults.anchor);
+  if (anchor !== undefined)
+    result.anchor = anchor;
+  let alignmentBaseline =
+    getFirstDefinedValue(result['alignment-baseline'],
+      defaults['alignment-baseline']);
+  if (alignmentBaseline !== undefined)
+    result['alignment-baseline'] = alignmentBaseline;
+  return result;
 }
 
 /**
@@ -55,34 +90,21 @@ export function getNormalizedLineStyleOptions({
  * 
  * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
  *   [options] - Options.
+ * @param {module:meteoJS/thermodynamicDiagram~lineOptions}
+ *   [defaults] - Optional defaults.
  * @returns {module:meteoJS/thermodynamicDiagram~lineOptions}
  *   Normalized options.
  * @internal
  */
 export function getNormalizedLineOptions({
-  visible = true,
+  visible = undefined,
   style = {}
-} = {}) {
+} = {}, defaults = {}) {
   return {
-    visible,
-    style: getNormalizedLineStyleOptions(style)
+    visible: getFirstDefinedValue(visible, defaults.visible, true),
+    style: getNormalizedLineStyleOptions(style, defaults.style)
   };
 }
-
-/**
- * Definition of font options.
- * 
- * @typedef {Object} module:meteoJS/thermodynamicDiagram~fontOptions
- * @param {} [fill=undefined] - Font color.
- * @param {} [size=undefined] - Size.
- * @param {} [family=undefined] - Family.
- * @param {} [anchor=undefined] - Anchor.
- * @param {} [leading=undefined] - Leading.
- * @param {} [stretch=undefined] - Stretch.
- * @param {} [style=undefined] - Style.
- * @param {} [variant=undefined] - Variant.
- * @param {} [weight=undefined] - Weight.
- */
 
 /**
  * A text with its visibility, style and font style.
@@ -98,6 +120,8 @@ export function getNormalizedLineOptions({
  * 
  * @param {module:meteoJS/thermodynamicDiagram~textOptions}
  *   [options] - Options.
+ * @param {module:meteoJS/thermodynamicDiagram~textOptions}
+ *   [defaults] - Optional defaults.
  * @returns {module:meteoJS/thermodynamicDiagram~textOptions}
  *   Normalized options.
  * @internal
@@ -105,10 +129,10 @@ export function getNormalizedLineOptions({
 export function getNormalizedTextOptions({
   visible = true,
   font = {}
-} = {}) {
+} = {}, defaults = {}) {
   return {
-    visible,
-    font
+    visible: getFirstDefinedValue(visible, defaults.visible, true),
+    font: getNormalizedFontOptions(font, defaults.font)
   };
 }
 
@@ -126,6 +150,8 @@ export function getNormalizedTextOptions({
  * 
  * @param {module:meteoJS/thermodynamicDiagram~lineTextOptions}
  *   [options] - Options.
+ * @param {module:meteoJS/thermodynamicDiagram~lineTextOptions}
+ *   [defaults] - Optional defaults.
  * @returns {module:meteoJS/thermodynamicDiagram~lineTextOptions}
  *   Normalized options.
  * @internal
@@ -134,48 +160,67 @@ export function getNormalizedLineTextOptions({
   visible = true,
   style = {},
   font = {}
-} = {}) {
+} = {}, defaults = {}) {
   return {
-    visible,
-    style: getNormalizedLineStyleOptions(style),
-    font
+    visible: getFirstDefinedValue(visible, defaults.visible, true),
+    style: getNormalizedLineStyleOptions(style, defaults.style),
+    font: getNormalizedFontOptions(font, defaults.font)
   };
+}
+
+/**
+ * Returns the first defined argument.
+ * 
+ * @param {mixed|undefined} [...params] - Some values.
+ * @returns {mixed|undefined} - First defined value, if available.
+ */
+export function getFirstDefinedValue(...params) {
+  return params
+    .reduce((acc, cur) => { return (acc !== undefined) ? acc : cur; });
 }
 
 /**
  * Draws text in a SVG node.
  * 
  * @param {Object} options - Options.
- * @param {external:SVG} node - SVG node.
- * @param {string} text - Text.
- * @param {number} x - X coordinate.
- * @param {number} y - Base Y coordinate.
- * @param {number} [horizontalPadding=3] - Padding in x direction.
- * @param {module:meteoJS/thermodynamicDiagram~fontOptions} [font] - Font style.
- * @prarm {string|undefined} [fillColor='white'] - Background fill color.
+ * @param {external:SVG} options.node - SVG node.
+ * @param {string} options.text - Text.
+ * @param {number} options.x - X coordinate.
+ * @param {number} options.y - Base Y coordinate.
+ * @param {number} [options.horizontalMargin=0] - Padding in x direction.
+ * @param {number} [options.verticalMargin=0] - Padding in y direction.
+ * @param {module:meteoJS/thermodynamicDiagram~fontOptions} [options.font] - Font style.
+ * @prarm {string|Object} [options.fill] - Fill for background.
+ * @returns {external:SVG} - SVG group containing the inserted elements.
  */
 export function drawTextInto({
   node,
   text,
   x,
   y,
-  horizontalPadding = 3,
+  horizontalMargin = 0,
+  verticalMargin = 0,
   font = {},
-  fillColor = 'white'
+  fill = {}
 }) {
-  const background = node.rect().fill(fillColor);
+  const group = node.group();
+  if (!('color' in fill))
+    fill.color = 'white';
+  const background = node.rect().fill(fill);
   const textNode = node
     .text(text)
     .attr({ x, y })
     .font(font);
   if (font['alignment-baseline'] == 'bottom')
-    textNode.dy(-textNode.bbox().height);
+    textNode.dy(-textNode.bbox().height - 5);
   textNode
-    .dx(horizontalPadding * ((textNode.attr('text-anchor') == 'end') ? -1 : 1));
+    .dx(horizontalMargin * ((textNode.attr('text-anchor') == 'end') ? -1 : 1))
+    .dy(verticalMargin * ((font['alignment-baseline'] == 'bottom') ? -1 : 1));
   background.attr({
     x: textNode.bbox().x,
     y: textNode.bbox().y,
     width: textNode.bbox().width,
     height: textNode.bbox().height
   });
+  return group;
 }
---
+++ test/meteoJS/thermodynamicDiagram/functions.test.js
@@ -0,0 +1,73 @@
+﻿import assert from 'assert';
+import {
+  getFirstDefinedValue,
+  getNormalizedLineStyleOptions,
+  getNormalizedFontOptions
+} from '../../../src/meteoJS/thermodynamicDiagram/Functions.js';
+
+describe('Function tests', () => {
+  it('getFirstDefinedValue', () => {
+    assert.equal(getFirstDefinedValue(undefined), undefined, 'undefined');
+    assert.equal(getFirstDefinedValue(1), 1, '1');
+    assert.equal(getFirstDefinedValue('A'), 'A', 'A');
+    assert.equal(Object.keys(getFirstDefinedValue({ a: 1, b: 2 })).length, 2, 'Object');
+    assert.equal(getFirstDefinedValue({ a: 1, b: 2 }).a, 1, 'Object');
+    assert.equal(getFirstDefinedValue({ a: 1, b: 'c' }).b, 'c', 'Object');
+    assert.equal(getFirstDefinedValue(undefined, undefined), undefined, 'undefined');
+    assert.equal(getFirstDefinedValue(1, undefined), 1, '1');
+    assert.equal(getFirstDefinedValue('A', undefined), 'A', 'A');
+    assert.equal(getFirstDefinedValue(undefined, 1), 1, '1');
+    assert.equal(getFirstDefinedValue(undefined, 'A'), 'A', 'A');
+    assert.equal(getFirstDefinedValue(1, 'A'), 1, '1');
+    assert.equal(Object.keys(getFirstDefinedValue({ a: 1, b: 'c' }, { c: 1 }, { d: 'e', f: 10, g: 100 })).length,
+      2, 'Object');
+    assert.equal(Object.keys(getFirstDefinedValue(undefined, { c: 1 }, { d: 'e', f: 10, g: 100 })).length,
+      1, 'Object');
+  });
+  it('getNormalizedLineStyleOptions', () => {
+    const empty = getNormalizedLineStyleOptions();
+    assert.equal(Object.keys(empty).length, 2, 'empty');
+    assert.equal(empty.color, 'black', 'empty');
+    assert.equal(empty.width, 1, 'empty');
+    const red = getNormalizedLineStyleOptions({ color: 'red' });
+    assert.equal(Object.keys(red).length, 2, 'red');
+    assert.equal(red.color, 'red', 'red');
+    assert.equal(red.width, 1, 'red');
+    const dasharray = getNormalizedLineStyleOptions({ dasharray: 3 });
+    assert.equal(Object.keys(dasharray).length, 3, 'dasharray');
+    assert.equal(dasharray.color, 'black', 'dasharray');
+    assert.equal(dasharray.width, 1, 'dasharray');
+    assert.equal(dasharray.dasharray, 3, 'dasharray');
+    const green = getNormalizedLineStyleOptions({ color: 'green', dasharray: 3 });
+    assert.equal(Object.keys(green).length, 3, 'green');
+    assert.equal(green.color, 'green', 'green');
+    assert.equal(green.width, 1, 'green');
+    assert.equal(green.dasharray, 3, 'green');
+  });
+  it('getNormalizedFontOptions', () => {
+    const empty = getNormalizedFontOptions();
+    assert.equal(Object.keys(empty).length, 2, 'empty');
+    assert.equal(empty.size, 12, 'empty');
+    assert.equal(empty.color, 'black', 'empty');
+    const red = getNormalizedFontOptions({ color: 'red' });
+    assert.equal(Object.keys(red).length, 2, 'red');
+    assert.equal(red.size, 12, 'red');
+    assert.equal(red.color, 'red', 'red');
+    const anchor = getNormalizedFontOptions({ color: 'red' }, { anchor: 'start' });
+    assert.equal(Object.keys(anchor).length, 3, 'anchor');
+    assert.equal(anchor.size, 12, 'anchor');
+    assert.equal(anchor.color, 'red', 'anchor');
+    assert.equal(anchor.anchor, 'start', 'anchor');
+    const alignment = getNormalizedFontOptions({ color: 'red' }, { 'alignment-baseline': 'bottom', fill: 'white' });
+    assert.equal(Object.keys(alignment).length, 3, 'alignment');
+    assert.equal(alignment.size, 12, 'alignment');
+    assert.equal(alignment.color, 'red', 'alignment');
+    assert.equal(alignment['alignment-baseline'], 'bottom', 'alignment');
+    const fill = getNormalizedFontOptions({ color: 'red', fill: 'white' }, { 'alignment-baseline': 'bottom' });
+    assert.equal(Object.keys(fill).length, 4, 'fill');
+    assert.equal(fill.size, 12, 'fill');
+    assert.equal(fill.color, 'red', 'fill');
+    assert.equal(fill['alignment-baseline'], 'bottom', 'fill');
+    assert.equal(fill.fill, 'white', 'fill');
+  });
+});
