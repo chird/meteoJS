f6dd40c89239706bffb7e5a6270fe0d8b0fe70cf
Ivo Sonderegger <ivo.sonderegger@srf.ch>
2020-07-07 14:42:32 +0200

    Removed unused package

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const packageJSON = require("./package.json");
 
 module.exports = {
